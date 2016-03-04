648f2a4973b89ae2b6b5e01d87b7977b9e65179f
Alex Richardson <arichardson.kde@gmail.com>
2016-03-04 14:08:56 +0000

    Fix the links in the sysroot
    We should now be creating the same thing as the build_sdk.sh script
    with a few extra files
    
    Almost finished #4

--- cheribuild.py
+++ cheribuild.py
@@ -874,8 +874,6 @@ class BuildSDK(Project):
             runCmd("ln", "-fs", tool, "mips4-unknown-freebsd-" + tool, cwd=self.config.sdkDir / "bin")
             runCmd("ln", "-fs", tool, "mips64-unknown-freebsd-" + tool, cwd=self.config.sdkDir / "bin")
 
-        # TODO: fix symlinks
-        # TODO: add cheridis
         # Compile the cheridis helper (TODO: add it to the LLVM repo instead?)
         cheridisSrc = """
 #include <stdio.h>
@@ -905,6 +903,69 @@ int main(int argc, char** argv)
 }"""
         runCmd("cc", "-DLLVM_PATH=\"%s\"" % str(self.config.sdkDir / "bin"), "-x", "c", "-",
                "-o", self.config.sdkDir / "bin/cheridis", input=cheridisSrc)
+
+        # fix symbolic links in the sysroot:
+        print("Fixing absolute paths in symbolic links inside lib directory...")
+        fixlinksSrc = """
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sysexits.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    DIR *dir = opendir(".");
+    struct dirent *file;
+    char *dirname;
+    int links = 0, fixed = 0;
+
+    while ((file = readdir(dir)) != NULL)
+    {
+        char target[1024];
+        ssize_t index =
+            readlink(file->d_name, target, sizeof(target) - 1);
+
+        if (index < 0) {
+            // Not a symlink?
+            if (errno == EINVAL)
+                continue;
+
+            err(EX_OSERR, "error in readlink('%s')", file->d_name);
+        }
+
+        links++;
+
+        // Fix absolute paths.
+        if (target[0] == '/') {
+            target[index] = 0;
+
+            char *newName;
+            asprintf(&newName, "../..%s", target);
+
+            if (unlink(file->d_name))
+                err(EX_OSERR, "Failed to remove old link");
+
+            if (symlink(newName, file->d_name))
+                err(EX_OSERR, "Failed to create link");
+            free(newName);
+            fixed++;
+        }
+    }
+    closedir(dir);
+
+    if (links == 0)
+        errx(EX_USAGE, "no symbolic links in %s", getwd(NULL));
+
+    printf("fixed %d/%d symbolic links\\n", fixed, links);
+}
+"""
+        runCmd("cc", "-x", "c", "-", "-o", self.config.sdkDir / "bin/fixlinks", input=fixlinksSrc)
+        runCmd(self.config.sdkDir / "bin/fixlinks", cwd=self.config.sdkSysrootDir / "usr/lib")
         print("Successfully populated sysroot")
 
 
