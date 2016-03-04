b5aea8b22e4dda77ed2f7aee3e8d54a69bc462f6
Alex Richardson <arichardson.kde@gmail.com>
2016-03-04 12:57:08 +0000

    Add the cheridis binary to the SDK
    One more step to getting #4 done

--- cheribuild.py
+++ cheribuild.py
@@ -876,6 +876,35 @@ class BuildSDK(Project):
 
         # TODO: fix symlinks
         # TODO: add cheridis
+        # Compile the cheridis helper (TODO: add it to the LLVM repo instead?)
+        cheridisSrc = """
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    int i;
+    int byte;
+
+    FILE *dis = popen(LLVM_PATH "llvm-mc -disassemble -triple=cheri-unknown-freebsd", "w");
+    for (i=1 ; i<argc ; i++)
+    {
+        char *inst = argv[i];
+        if (strlen(inst) == 10)
+        {
+            if (inst[0] != '0' || inst[1] != 'x') continue;
+            inst += 2;
+        }
+        else if (strlen(inst) != 8) continue;
+        for (byte=0 ; byte<8 ; byte+=2)
+        {
+            fprintf(dis, "0x%.2s ", &inst[byte]);
+        }
+    }
+    pclose(dis);
+}"""
+        runCmd("cc", "-DLLVM_PATH=\"%s\"" % str(self.config.sdkDir / "bin"), "-x", "c", "-",
+               "-o", self.config.sdkDir / "bin/cheridis", input=cheridisSrc)
         print("Successfully populated sysroot")
 
 
