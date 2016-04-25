f65b0f831a80e0a2b47c8ab4a47acf771aed2cba
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-04-25 15:04:29 +0100

    minor adjustment to comments

--- py3-run-remote.sh
+++ py3-run-remote.sh
@@ -6,10 +6,10 @@ fi
 host="$1"
 script="$2"
 shift 2
-# Unfortunately this doesn't work propertly, ctrl+C won't kill the script
+# Unfortunately the following line doesn't work propertly, ctrl+C won't kill the script
 # it will only the ssh process as there is no tty (because stdin is a file)
-# ssh -t "$host" python3 - < "$script" "$@"
+# `ssh -t "$host" python3 - < "$script" "$@"`
 # and even if we force a tty with -tt it won't work because python3 will start in interpreter mode...
-# ssh -tt "$host" python3 - < "$script" "$@"
+# `ssh -tt "$host" python3 - < "$script" "$@"`
 # so the only solution seems to be scp script to host and run it there
 scp "$script" "${host}:~/.remote-py3-script.py" > /dev/null && ssh -tt "$host" python3 '$HOME/.remote-py3-script.py' "$@"
