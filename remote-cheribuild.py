349adb186a88fc8921619a4210b2aae362a08a85
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-04-25 15:07:20 +0100

    Add remote-cheribuild.py
    Usage: remote-cheribuild.py [host] [cheribuild.py arguments]
    
    This script uses combine-files.py to combine the module into a single
    file and then py3-run-remote.sh to run in on the remote host via SSH

---
+++ remote-cheribuild.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import sys
+import tempfile
+from pathlib import Path
+
+scriptDir = Path(__file__).resolve().parent  # type: Path
+host = sys.argv[1]
+cheribuildArgs = sys.argv[2:]
+
+with tempfile.NamedTemporaryFile(prefix="cheribuild-", suffix=".py") as tmp:
+    combineScript = scriptDir / "combine-files.py"
+    assert combineScript.is_file()
+    subprocess.check_call(["python3", str(combineScript)], stdout=tmp)
+    print("About to run cheribuild on host '" + host + "' with the following arguments:", cheribuildArgs)
+    print("Note: file that will be run is located at", tmp.name)
+    input("Press enter to continue...")
+    subprocess.check_call([str(scriptDir / "py3-run-remote.sh"), host, tmp.name] + cheribuildArgs)
