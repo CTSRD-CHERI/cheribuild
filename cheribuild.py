a06344a0fd9340a26e88d5ba1d8b5657af555a93
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-05-03 12:57:28 +0100

    Don't print full python backtrace when user presses Ctrl+C

--- cheribuild.py
+++ cheribuild.py
@@ -8,5 +8,8 @@ env = os.environ.copy()
 scriptDir = Path(__file__).resolve().parent  # type: Path
 env["PYTHONPATH"] = str(scriptDir)
 
-# just run the module and return the corresponding exit code
-sys.exit(subprocess.call(["python3", "-b", "-Wall", "-m", "pycheribuild"] + sys.argv[1:], env=env))
+try:
+    # just run the module and return the corresponding exit code
+    sys.exit(subprocess.call(["python3", "-b", "-Wall", "-m", "pycheribuild"] + sys.argv[1:], env=env))
+except KeyboardInterrupt:
+    sys.exit("Exiting due to Ctrl+C")
