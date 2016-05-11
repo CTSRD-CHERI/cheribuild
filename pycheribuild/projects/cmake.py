e5521ebb4d3f685d507cb099b551b6562c277894
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2016-05-11 13:42:32 +0100

    Use the correct branch for CMake

--- pycheribuild/projects/cmake.py
+++ pycheribuild/projects/cmake.py
@@ -8,5 +8,5 @@ class BuildCMake(AutotoolsProject):
         super().__init__(config, installDir=config.otherToolsDir, configureScript="bootstrap",
                          # gitUrl="https://cmake.org/cmake.git")
                          gitUrl="https://github.com/Kitware/CMake")  # a lot faster than the official repo
-        self.gitBranch = "release"  # track the stable release branch
+        self.gitBranch = "maint"  # track the stable release branch - which is not "release" (see CMake wiki))
         # TODO: do we need to use gmake on FreeBSD?
