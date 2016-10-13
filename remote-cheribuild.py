80ac0a560e6a79a7089651a888f73a341e3193bf
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-10-13 14:08:30 +0100

    No longer call the shell script in remote-cheribuil.py, do it in python

--- remote-cheribuild.py
+++ remote-cheribuild.py
@@ -16,4 +16,17 @@ with tempfile.NamedTemporaryFile(prefix="cheribuild-", suffix=".py") as tmp:
     print("About to run cheribuild on host '" + host + "' with the following arguments:", cheribuildArgs)
     print("Note: file that will be run is located at", tmp.name)
     input("Press enter to continue...")
-    subprocess.check_call([str(scriptDir / "py3-run-remote.sh"), host, tmp.name] + cheribuildArgs)
+    # the bash script:
+    """
+# Unfortunately the following line doesn't work propertly, ctrl+C won't kill the script
+# it will only the ssh process as there is no tty (because stdin is a file)
+# `ssh -t "$host" python3 - < "$script" "$@"`
+# and even if we force a tty with -tt it won't work because python3 will start in interpreter mode...
+# `ssh -tt "$host" python3 - < "$script" "$@"`
+# so the only solution seems to be scp script to host and run it there
+scp "$script" "${host}:~/.remote-py3-script.py" > /dev/null && ssh -tt "$host" python3 '$HOME/.remote-py3-script.py' "$@"
+    """
+    remoteFile = "$HOME/.remote-py3-script.py"
+    subprocess.check_call(["scp", tmp.name, host + ":" + remoteFile])
+    # call execvp so that we get "^CExiting due to Ctrl+C" instead of a CalledProcessError
+    os.execvp("ssh", ["ssh", "-tt", host, "--", "python3", remoteFile] + cheribuildArgs)
