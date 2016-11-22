c3ed45e0120fd36c1a74b7e9486f7b191346eed2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2016-11-22 18:22:19 +0000

    Refactor elftoolchain build

--- pycheribuild/projects/elftoolchain.py
+++ pycheribuild/projects/elftoolchain.py
@@ -1,11 +1,14 @@
 from ..project import Project
 from ..utils import *
 
+import pwd
+import grp
+import os
+
 
 class BuildElfToolchain(Project):
-    def __init__(self, config: CheriConfig, gitUrl="https://github.com/emaste/elftoolchain.git"):
-        super().__init__(config, installDir=config.sdkDir,
-                         gitUrl=gitUrl)
+    def __init__(self, config: CheriConfig, gitUrl="https://github.com/emaste/elftoolchain.git", **kwargs):
+        super().__init__(config, installDir=config.sdkDir, gitUrl=gitUrl, **kwargs)
         self.buildDir = self.sourceDir
         if IS_LINUX:
             self._addRequiredSystemTool("bmake")
@@ -17,15 +20,21 @@ class BuildElfToolchain(Project):
         # self.makeArgs = ["WITH_TESTS=no", "-DNO_ROOT"]
         # TODO: build static?
         self.commonMakeArgs.append("WITH_TESTS=no")
-        self.commonMakeArgs.append("LDSTATIC=-static")
+        self.commonMakeArgs.append("WITH_DOCUMENTATION=no")
+        self.programsToBuild = ["brandelf", "ar"]
+        if not self.config.verbose:
+            self.commonMakeArgs.append("-s")
 
     def compile(self):
-        targets = ["common", "libelf", "libelftc", "libpe"]
+        libTargets = ["common", "libelf", "libelftc", "libpe", "libdwarf"]
         # tools that we want to build:
-        targets += ["brandelf"]
-        for tgt in targets:
-            self.runMake(self.commonMakeArgs + [self.config.makeJFlag],
-                         "all", cwd=self.sourceDir / tgt, logfileName="build." + tgt)
+        # build is not parallel-safe -> we can't make with all the all-foo targets and -jN
+        # To speed it up run make for the individual library directories instead and then for all the binaries
+        for tgt in libTargets:
+            self.runMake(self.commonMakeArgs + [self.config.makeJFlag], makeTarget="all-" + tgt,
+                         logfileName="build." + tgt)
+        progTargets = list(map(lambda p: "all-" + p, self.programsToBuild))
+        self.runMake(self.commonMakeArgs + [self.config.makeJFlag] + progTargets, logfileName="build.programs")
 
     def install(self):
         if IS_FREEBSD:
