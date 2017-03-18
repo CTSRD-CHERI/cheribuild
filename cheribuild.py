3f8124f45b383ecfe5accb98a6325c8a4f45b870
Alex Richardson <alexander.richardson@cl.cam.ac.uk>
2017-03-18 12:37:13 +0000

    Fix running symlinks to cheribuild.py with python 3.6
    I have a symlink to cheribuild.py in ~/bin and for some reason python
    only looks for module imports in the directory where the symlink is
    and not the resolved file. This seems to be a change in python 3.6
    as https://hg.python.org/cpython/rev/47c31e7d3779 suggests this should
    work as expected.

--- cheribuild.py
+++ cheribuild.py
@@ -32,9 +32,10 @@
 
 # https://stackoverflow.com/questions/1112618/import-python-package-from-local-directory-into-interpreter
 # https://stackoverflow.com/questions/14500183/in-python-can-i-call-the-main-of-an-imported-module
-try:
-    from .pycheribuild import __main__   # "myapp" case
-except SystemError:
-    import pycheribuild.__main__  # "__main__" case
+from pathlib import Path
+import sys
+module_dir = Path(__file__).resolve().parent
+sys.path.append(str(module_dir))
+from pycheribuild import main  # "__main__" case
 
-pycheribuild.__main__.main()
+main()
