0c80ba105513545b15cf2613b21bbbeae74bd339
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-16 22:03:43 +0100

    The CMake project seems to have gotten rid of the "maint" branch.
    Instead, we can track the "release" branch on GitHub, as opposed
    to the master branch that is otherwise available.

--- pycheribuild/projects/cmake.py
+++ pycheribuild/projects/cmake.py
@@ -35,7 +35,7 @@ from ..utils import *
 class BuildCMake(AutotoolsProject):
     # repository = "https://cmake.org/cmake.git"
     repository = "https://github.com/Kitware/CMake"  # a lot faster than the official repo
-    gitBranch = "maint"  # track the stable release branch - which is not "release" (see CMake wiki))
+    gitBranch = "release"  # track the stable release branch
     defaultInstallDir = AutotoolsProject._installToBootstrapTools
 
     def __init__(self, config: CheriConfig):
