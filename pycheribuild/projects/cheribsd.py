01c746a0905946766f6fbf521977595a32a44f80
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-10-25 10:13:18 +0100

    Fix external-toolchain world builds

--- pycheribuild/projects/cheribsd.py
+++ pycheribuild/projects/cheribsd.py
@@ -272,7 +272,8 @@ class _BuildFreeBSD(Project):
             self.useExternalToolchainForWorld = True
             self.useExternalToolchainForKernel = True
             self.linkKernelWithLLD = True
-            self.cross_toolchain_config.add(XAS="/usr/bin/as")  # TODO: would be nice if we could compile the asm with clang
+            # DONT SET XAS!!! It prevents bfd from being built
+            # self.cross_toolchain_config.add(XAS="/usr/bin/as")  # TODO: would be nice if we could compile the asm with clang
 
         elif self.mipsToolchainPath:
             cross_prefix = str(self.mipsToolchainPath / "bin") + "/"
@@ -288,6 +289,7 @@ class _BuildFreeBSD(Project):
             # Won't compile with CHERI clang yet
             self.cross_toolchain_config.add(RESCUE=False)
             self.cross_toolchain_config.add(BOOT=False)  # bootloaders won't link with LLD yet
+            # DONT SET XAS!!! It prevents bfd from being built
             self.cross_toolchain_config.add(XAS=cross_prefix + "clang " + target_flags)
         else:
             fatalError("Invalid state, should have a cross env")
