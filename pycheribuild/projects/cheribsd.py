7bda9973247fe6d51b8dd4ab6481ea419d555274
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-02-23 13:16:22 +0000

    When building CheriBSD with debug info don't split it from the binary
    This means we can just scp the file over to a cheribsd instace, run gdb
    and get symbols and sources. Otherwise we also have to find the .debug file
    and copy it to the right location.

--- pycheribuild/projects/cheribsd.py
+++ pycheribuild/projects/cheribsd.py
@@ -202,6 +202,9 @@ class _BuildFreeBSD(Project):
 
         if self.addDebugInfoFlag:
             self.make_args.set(DEBUG_FLAGS="-g")
+            # Don't split the debug info from the binary, just keep it as part of the binary
+            # This means we can just scp the file over to a cheribsd instace, run gdb and get symbols and sources.
+            self.make_args.set_with_options(DEBUG_FILES=False)
 
         # tests off by default because they take a long time and often seems to break
         # the creation of disk-image (METALOG is invalid)
