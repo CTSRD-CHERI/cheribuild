CTSRD-CHERI/cheribuild

commit 759a9a2c4a3561f45ac83b41b8b727bd5d563e8f

Get better install instructions for opensuse systems
We can use the command-not-found tool to print the install intructions:

```
Possible solution: The program 'pkg-config' can be found in the following package:
  * pkg-config [ path: /usr/bin/pkg-config, repository: zypp (oss) ]

Try installing with:
    zypper install pkg-config
```

pycheribuild/projects/build_qemu.py
@@ -79,10 +79,12 @@ class BuildQEMU(AutotoolsProject):
             extraCFlags += " " + glibIncludes
 
 
-        ccinfo = getCompilerInfo(os.getenv("CC", shutil.which("cc")))
-        if ccinfo.compiler == "apple-clang" or (ccinfo.compiler == "clang" and ccinfo.version >= (4, 0, 0)):
-            # silence this warning that comes lots of times (it's fine on x86)
-            extraCFlags += " -Wno-address-of-packed-member"
+        compiler = os.getenv("CC", shutil.which("cc"))
+        if compiler:
+            ccinfo = getCompilerInfo()
+            if ccinfo.compiler == "apple-clang" or (ccinfo.compiler == "clang" and ccinfo.version >= (4, 0, 0)):
+                # silence this warning that comes lots of times (it's fine on x86)
+                extraCFlags += " -Wno-address-of-packed-member"
         if self.config.unified_sdk:
             targets = "cheri256-softmmu,cheri128-softmmu,cheri128magic-softmmu"
         else:

pycheribuild/projects/project.py
@@ -206,7 +206,7 @@ class SimpleProject(FileSystemUtils, metaclass=ProjectSubclassDefinitionHook):
     def _addRequiredSystemTool(self, executable: str, installInstructions=None, freebsd: str=None, apt: str = None,
                                zypper: str=None, homebrew: str=None, cheribuild_target: str=None):
         if not installInstructions:
-            installInstructions = OSInfo.install_instructions(executable, True, freebsd=freebsd, zypper=zypper, apt=apt,
+            installInstructions = OSInfo.install_instructions(executable, False, freebsd=freebsd, zypper=zypper, apt=apt,
                                                                homebrew=homebrew, cheribuild_target=cheribuild_target)
         self.__requiredSystemTools[executable] = installInstructions
 

pycheribuild/utils.py
@@ -380,7 +380,7 @@ class OSInfo(object):
 
     @classmethod
     def isSuse(cls):
-        return cls.__is_linux_distribution("suse")
+        return cls.__is_linux_distribution("suse") or cls.__is_linux_distribution("opensuse")
 
     @classmethod
     def isDebian(cls):
@@ -439,10 +439,22 @@ class OSInfo(object):
             else:
                 guessed_package = True
                 install_name = "lib" + name + "-dev" if is_lib else name
-        elif cls.uses_zypper() and zypper:
+        elif cls.uses_zypper():
             if zypper:
                 install_name = zypper
             else:
+                if not is_lib and shutil.which("command-not-found"):
+                    # for programs we can use the command-not-found tool to get detailed install instructions
+                    def command_not_found():
+                        hint = subprocess.getoutput(shutil.which("command-not-found") + " " + name)
+                        print(hint)
+                        if hint and not name + ": command not found" in hint:
+                            msg_start = hint.find("The program")
+                            if msg_start:
+                                hint = hint[msg_start:]
+                            return hint
+                        return "Could not find package for program " + name + ". Maybe `zypper in " + name + "` will work."
+                    return command_not_found
                 guessed_package = True
                 install_name = "lib" + name + "-devel" if is_lib else name
         else:
@@ -450,7 +462,7 @@ class OSInfo(object):
             install_name = name
         if guessed_package:
             # not sure if the package name is correct:
-            return "Possibly running`" + cls.package_manager() + " install " + install_name + \
+            return "Possibly running `" + cls.package_manager() + " install " + install_name + \
                                   "` fixes this. Note: package name may not be correct."
         else:
             return "Run `" + cls.package_manager() + " install " + install_name + "`"

commit a1f975e14dc315ebe2450a12d3f3658226ca1f9e

Fix error introduced in 759a9a2c4a3561f45ac83b41b8b727bd5d563e8f

pycheribuild/projects/build_qemu.py
@@ -81,7 +81,7 @@ class BuildQEMU(AutotoolsProject):
 
         compiler = os.getenv("CC", shutil.which("cc"))
         if compiler:
-            ccinfo = getCompilerInfo()
+            ccinfo = getCompilerInfo(compiler)
             if ccinfo.compiler == "apple-clang" or (ccinfo.compiler == "clang" and ccinfo.version >= (4, 0, 0)):
                 # silence this warning that comes lots of times (it's fine on x86)
                 extraCFlags += " -Wno-address-of-packed-member"