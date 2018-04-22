CTSRD-CHERI/cheribuild

commit 0e9c6d64f34420deee9872adf0e74385ff2e5e3c

Build gdb-native as part of the sdk
This required a bit of refactoring since gdb depended on sdk (even though
this is only the case for the MIPS version not the host one). To debug
dependency loops like this I added the new --print-targets-only flag
which prints which targets will be executed but unlike --pretend doesn't
print the commands that will be executed.

pycheribuild/config/chericonfig.py
@@ -89,6 +89,11 @@ class CheriConfig(object):
                                                     choices=("pcrel", "plt", "legacy", "fn-desc"),
                                                     help="The ABI to use for cap-table mode")
 
+
+        self.print_targets_only =  loader.addBoolOption("print-targets-only", helpHidden=True,
+                                                        help="Only print the targets that would be executed instead of"
+                                                             " all the commands")
+
         # Attributes for code completion:
         self.verbose = None  # type: bool
         self.quiet = None  # type: bool

pycheribuild/projects/cross/crosscompileproject.py
@@ -47,6 +47,13 @@ def _installDirMessage(project: "CrossCompileProject"):
     return "UNKNOWN"
 
 
+def crosscompile_dependencies(cls: "typing.Type[CrossCompileProject]"):
+    # TODO: can I make this take the class instance instead of the class
+    if cls.crossCompileTarget == CrossCompileTarget.NATIVE:
+        return ["freestanding-sdk"] if get_global_config().use_sdk_clang_for_native_xbuild else []
+    else:
+        return ["freestanding-sdk"] if cls.baremetal else ["cheribsd-sdk"]
+
 class CrossCompileMixin(object):
     doNotAddToTargets = True
     crossInstallDir = CrossInstallDir.CHERIBSD_ROOTFS
@@ -59,7 +66,7 @@ class CrossCompileMixin(object):
     supported_architectures = list(CrossCompileTarget)
     defaultInstallDir = ComputedDefaultValue(function=_installDir, asString=_installDirMessage)
     appendCheriBitsToBuildDir = True
-    dependencies = lambda cls: ["freestanding-sdk"] if cls.baremetal else ["cheribsd-sdk"]
+    dependencies = crosscompile_dependencies
     defaultLinker = "lld"
     baremetal = False
     forceDefaultCC = False  # for some reason ICU binaries build during build crash -> fall back to /usr/bin/cc there

pycheribuild/projects/sdk.py
@@ -51,7 +51,7 @@ class BuildSdk(TargetAliasWithDependencies):
 
 class BuildFreestandingSdk(SimpleProject):
     target = "freestanding-sdk"
-    dependencies = ["binutils", "llvm", "qemu"]  # TODO: should we add ld.bfd?
+    dependencies = ["binutils", "llvm", "qemu", "gdb-native"]  # TODO: should we add ld.bfd?
     dependenciesMustBeBuilt = True
 
     def __init__(self, config: CheriConfig):

pycheribuild/targets.py
@@ -226,6 +226,10 @@ class TargetManager(object):
             target.checkSystemDeps(config)
         # all dependencies exist -> run the targets
         for target in chosenTargets:
-            target.execute()
+            if config.print_targets_only:
+                statusUpdate("Will build target", coloured(AnsiColour.yellow, target.name))
+                print("    Dependencies for", target.name, "are", target.projectClass.allDependencyNames())
+            else:
+                target.execute()
 
 targetManager = TargetManager()

pycheribuild/utils.py
@@ -58,7 +58,7 @@ __all__ = ["typing", "IS_LINUX", "IS_FREEBSD", "IS_MAC", "printCommand", "includ
            "runCmd", "statusUpdate", "fatalError", "coloured", "AnsiColour", "setCheriConfig", "setEnv",  # no-combine
            "warningMessage", "Type_T", "typing", "popen_handle_noexec", "extract_version", "get_program_version", # no-combine
            "check_call_handle_noexec", "ThreadJoiner", "getCompilerInfo", "latestClangTool",  # no-combine
-           "defaultNumberOfMakeJobs", "commandline_to_str", "OSInfo", "is_jenkins_build"]  # no-combine
+           "defaultNumberOfMakeJobs", "commandline_to_str", "OSInfo", "is_jenkins_build", "get_global_config"]  # no-combine
 
 
 if sys.version_info < (3, 4):
@@ -96,6 +96,12 @@ def is_jenkins_build() -> bool:
 def setCheriConfig(c: "CheriConfig"):
     global _cheriConfig
     _cheriConfig = c
+
+
+def get_global_config() -> "CheriConfig":
+    global _cheriConfig
+    assert _cheriConfig is not None
+    return _cheriConfig
 
 
 def __filterEnv(env: dict) -> dict:

commit 645a25144153d0bb1b08e9916ac3f79d76444be6

Unbreak --help
Bisecting shows that it was broken in 0e9c6d64f34420deee9872adf0e74385ff2e5e3c.
For some reason if I make --print-targets-only a hidden option it causes
argparse to assert during --help. To workaround this just make it a
visible by default option instead since I don't have time to debug the root
cause.

pycheribuild/config/chericonfig.py
@@ -89,9 +89,8 @@ class CheriConfig(object):
                                                     choices=("pcrel", "plt", "legacy", "fn-desc"),
                                                     help="The ABI to use for cap-table mode")
 
-        self.print_targets_only = loader.addBoolOption("print-targets-only", helpHidden=True,
-                                                        help="Only print the targets that would be executed instead of"
-                                                             " all the commands")
+        self.print_targets_only = loader.addBoolOption("print-targets-only", helpHidden=False,
+           help="Don't run the build but instead only print the targets that would be executed")
 
         # Attributes for code completion:
         self.verbose = None  # type: bool