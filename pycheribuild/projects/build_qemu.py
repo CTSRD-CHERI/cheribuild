740915182d95242778c7d3110eff7721a0d0eb28
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2018-06-12 13:50:41 +0100

    Add option for the qemu build to set the path of the python interpreter required by the qemu configure script.
    This allows to build qemu more easily on machines that use python3 as their default python version.

--- pycheribuild/projects/build_qemu.py
+++ pycheribuild/projects/build_qemu.py
@@ -53,6 +53,8 @@ class BuildQEMU(AutotoolsProject):
                                           default=True)
         cls.lto = cls.addBoolOption("use-lto", showHelp=True,
                                     help="Try to build QEMU with link-time optimization if possible", default=True)
+        cls.python_path = cls.addConfigOption("python", showHelp=True,
+                                              help="Path to python2 interpreter (required to build qemu)")
 
     @classmethod
     def qemu_binary(cls, config: CheriConfig):
@@ -134,6 +136,8 @@ class BuildQEMU(AutotoolsProject):
             "--cxx=" + str(self.config.clangPlusPlusPath),
             "--cc=" + str(self.config.clangPath),
             ])
+        if self.python_path:
+            self.configureArgs.extend(["--python={}".format(self.python_path)])
         # the capstone disassembler doesn't support CHERI instructions:
         self.configureArgs.append("--disable-capstone")
         if extraLDFlags:
