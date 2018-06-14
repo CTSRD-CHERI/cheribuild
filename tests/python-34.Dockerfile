20b6c948ff6f41d5cde4d79b57d29092688d6ac8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-06-14 14:27:38 +0100

    Revert "Drop python 3.4 support"
    It should work again. I was able to reproduce the jenkins error with
    python 3.5 so I won't drop support yet
    
    This reverts commit 2912893c3e48c39e64566398a09706bc7cfbf0ae.

---
+++ tests/python-34.Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.4
+
+LABEL maintainer="Alexander.Richardson@cl.cam.ac.uk"
+
+RUN pip install pytest
--- tests/run_jenkins_tests.sh
+++ tests/run_jenkins_tests.sh
@@ -2,7 +2,9 @@
 
 pytest_binary="python3 -m pytest"
 
-if [[ "$1" = "3.5.0" ]]; then
+if [[ "$1" = "3.4.0" ]]; then
+    test_prefix="3.4.0"
+elif [[ "$1" = "3.5.0" ]]; then
     test_prefix="3.5.0"
 elif [[ "$1" = "3.6" ]]; then
     test_prefix="3.6"
