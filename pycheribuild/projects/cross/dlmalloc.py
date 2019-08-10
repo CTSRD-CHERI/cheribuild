5a70793cec5584aaa9f7919cef1d055a487fb365
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2019-08-10 16:39:44 +0100

    Initial dlmalloc support

---
+++ pycheribuild/projects/cross/dlmalloc.py
@@ -0,0 +1,92 @@
+#
+# Copyright (c) 2019 Nathaniel Filardo
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory (Department of Computer Science and
+# Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+# DARPA SSITH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+import shutil
+
+from .crosscompileproject import *
+from ...utils import setEnv
+
+class SNMalloc(CrossCompileProject):
+    projectName = "dlmalloc"
+    repository = GitRepository("https://github.com/brooksdavis/dlmalloc_nonreuse")
+    appendCheriBitsToBuildDir = True
+    supported_architectures = [CrossCompileTarget.CHERI, CrossCompileTarget.NATIVE, CrossCompileTarget.MIPS]
+    defaultOptimizationLevel = ["-O2"]
+    make_kind = MakeCommandKind.GnuMake
+
+    @classmethod
+    def setupConfigOptions(cls, **kwargs):
+        super().setupConfigOptions(**kwargs)
+
+        cls.just_so          = cls.addBoolOption("just-so", help="Just build the .so shim")
+        cls.debug            = cls.addBoolOption("debug", help="Turn on debugging features")
+
+        cls.qmabs            = cls.addConfigOption("qmabs", kind=int,
+                                                   help="Quarantine memory absolute threshold")
+
+        cls.qmratio          = cls.addConfigOption("qmratio", kind=int,
+                                                   help="Quarantine memory ratio threshold")
+
+        cls.revoke           = cls.addBoolOption("revoke", help="Revoke quarantine before reusing")
+
+        cls.quar_unsafe      = cls.addBoolOption("unsafe-quarantine",
+                                                 help="Don't isolate quarantine structures")
+
+    def __init__(self, config: CheriConfig, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+
+    def compile(self, **kwargs):
+        if self.revoke :
+            self.CFLAGS.append("-DCAPREVOKE")
+
+        if self.qmabs :
+            self.CFLAGS.append("-DDEFAULT_MAX_FREEBUFBYTES=%d" % self.qpathresh)
+
+        if self.qmratio :
+            self.CFLAGS.append("-DDEFAULT_FREEBUF_PERCENT=%f" % self.qmratio)
+
+        if not self.quar_unsafe :
+            self.CFLAGS.append("-DSAFE_FREEBUF") 
+
+        self.make_args.add_flags("-f", self.sourceDir / "Makefile.cheribuild")
+        self.make_args.set(DEBUG=self.debug)
+        self.make_args.set(CAPREVOKE=self.revoke)
+        self.make_args.set(SRCDIR=self.sourceDir)
+        self.CFLAGS.append("--sysroot=%s" % self.sdkSysroot)
+        with setEnv(CHERI_SDK=self.config.sdkDir,
+                    CC=self.config.sdkBinDir/"clang",
+                    CFLAGS=commandline_to_str(self.default_compiler_flags)):
+            if self.just_so :
+                self.runMake("libdlmalloc_nonreuse.so", cwd=self.buildDir)
+            else :
+                self.runMake("all", cwd=self.buildDir)
+
+    def install(*args, **kwargs):
+        pass
