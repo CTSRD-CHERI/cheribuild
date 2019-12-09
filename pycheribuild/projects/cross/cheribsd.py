75ebdbac091b5faf8a7721e7de17fa1e169e6e60
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-12-09 15:24:18 +0000

    CheriBSD: Install kernel debug files when crossbuilding
    The llvm-objdump bug that caused to kernel to not build was fixed ages
    ago so we can remove this hack.

--- pycheribuild/projects/cross/cheribsd.py
+++ pycheribuild/projects/cross/cheribsd.py
@@ -567,11 +567,6 @@ class BuildFreeBSD(BuildFreeBSDBase):
             kernel_options.set(LD=linker, XLD=linker, HACK_EXTRA_FLAGS="-shared " + fuse_ld_flag,
                                TRAMP_LDFLAGS=fuse_ld_flag)
             kernel_options.set_env(LDFLAGS=fuse_ld_flag, XLDFLAGS=fuse_ld_flag)
-        if self.crossbuild:
-            kernel_options.set_with_options(KERNEL_TRAMPOLINE=False)
-            kernel_options.remove_var("DEBUG")
-            kernel_options.remove_all(lambda s: s.startswith("DEBUG"))
-            kernel_options.set(INSTALL_NODEBUG=True)
         kernel_options.set(KERNCONF=kernconf)
         return kernel_options
 
