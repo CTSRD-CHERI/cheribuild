f433d56d598de8803b1c8c748433e7315df5074f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-12-19 14:25:27 +0100

    Load shared libraries in gdb-run.sh
    For some reason this no longer happens automatically so "bt" can't print
    anything useful.

--- pycheribuild/files/cheribsd/gdb-run.sh
+++ pycheribuild/files/cheribsd/gdb-run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Start GDB, send a "r" command, and on the next prompt a backtrace
 # This automates waiting for minutes until GDB has loaded symbols and then pressing r to actually start the program
-"${GDB:-gdb}" -ex=r -ex=bt --args "$@"
+"${GDB:-gdb}" -ex=r -ex=sharedlibrary -ex=bt --args "$@"
