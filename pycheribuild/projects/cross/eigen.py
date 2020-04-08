70c77cd9335250532568ca755f860aabf167e192
Michael Dodson <md403@cam.ac.uk>
2020-04-08 10:12:32 +0100

    remove unnecessary methods that just call super

--- pycheribuild/projects/cross/eigen.py
+++ pycheribuild/projects/cross/eigen.py
@@ -37,16 +37,3 @@ class BuildEigen(CrossCompileCMakeProject):
     repository = GitRepository("https://gitlab.com/libeigen/eigen.git")
     native_install_dir = DefaultInstallDir.IN_BUILD_DIRECTORY
     cross_install_dir = DefaultInstallDir.SYSROOT
-
-    # @classmethod
-    # def setup_config_options(cls, **kwargs):
-    #     super().setup_config_options(**kwargs)
-    
-    # def __init__(self, config: CheriConfig, *args, **kwargs):
-    #     super().__init__(config, *args, **kwargs)
-
-    # def compile(self, **kwargs):
-    #     return super().compile(**kwargs)
-
-    # def install(self, *args, **kwargs):
-    #     return super().install(*args, **kwargs)
