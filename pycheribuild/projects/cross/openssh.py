4ffa1f94905f2e7db502732d003e902b1fc3da6f
Brooks Davis <brooks@one-eyed-alien.net>
2020-05-09 01:03:39 +0100

    Checkpoint a fett-openssh-riscv64 target.
    One hardcoded path means it builds for noone else and it probably doesn't
    work due to a lack of rpath usage.

---
+++ pycheribuild/projects/cross/openssh.py
@@ -0,0 +1,70 @@
+#
+# Copyright (c) 2020 SRI International
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory (Department of Computer Science and
+# Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
+# DARPA SSITH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+from .crosscompileproject import *
+from .qt5 import BuildQtWebkit
+from ...utils import runCmd, IS_FREEBSD
+
+
+class BuildOpenSSH(CrossCompileAutotoolsProject):
+    # Just add add the FETT target below for now.
+    doNotAddToTargets = True
+
+    repository = GitRepository("https://github.com/CTSRD-CHERI/openssh-portable.git")
+
+    native_install_dir = DefaultInstallDir.IN_BUILD_DIRECTORY
+    cross_install_dir = DefaultInstallDir.ROOTFS
+    # LD is used with CFLAGS so don't set to ld/ld.lld
+    _define_ld = False
+
+    def __init__(self, config: CheriConfig):
+        super().__init__(config)
+        self.addRequiredSystemTool("autoreconf", homebrew="autoconf")
+
+    def configure(self, **kwargs):
+        self.add_configure_env_arg("AR", self.target_info.ar)
+        self.add_configure_env_arg("DESTDIR", self.destdir)
+        self.run_cmd("autoreconf", str(self.sourceDir), cwd=self.buildDir)
+        super().configure(**kwargs)
+
+class BuildFettOpenSSH(BuildOpenSSH):
+    target = "fett-openssh"
+    project_name = "fett-openssh"
+    repository = GitRepository("https://github.com/CTSRD-CHERI/openssh-portable.git",
+                               default_branch="fett")
+
+    dependencies = ["fett-zlib", "fett-openssl"]
+
+    def configure(self, **kwargs):
+        self.configureArgs.append("--with-ssl-dir=/home/bed22/tmp/cheri/rootfs-riscv64/opt/riscv64/fett-openssl")
+        # TODO: use the right path
+        # TODO: add zliv
+        # TODO: add -Wl,rpath,/opt/<foo>/fett-openssl
+        super().configure(**kwargs)
