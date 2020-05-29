b6f9a99a88fff47ec2e7de55f48379942dd205a8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-05-29 20:14:43 +0100

    Try to fix jenkins

--- pycheribuild/projects/cross/cheribsd.py
+++ pycheribuild/projects/cross/cheribsd.py
@@ -816,6 +816,8 @@ class BuildFreeBSD(BuildFreeBSDBase):
                 self.run_make("installworld", options=install_world_args)
                 self.run_make("distribution", options=install_world_args)
                 if self.has_installsysroot_target:
+                    if is_jenkins_build():
+                        installsysroot_args.set_env(DESTDIR=self.target_info.sysroot_dir)
                     self.run_make("installsysroot", options=installsysroot_args)
 
         if skip_kernel:
