d9e7261246533302f2eb843b1a7c5d70315535b5
dodsonmg <43888469+dodsonmg@users.noreply.github.com>
2020-06-10 12:27:01 +0100

    ROS2 testing (#114)
    * fix bash command for echoing LD_LIBRARY_PATH
    
    * add run_test method to ros2 target
    
    * remove spurious return statements and add some comments
    
    * get ld cheri library path set correctly
    
    * attempting to link librt.so and failing
    
    * clean up set_env()
    
    * Update test-scripts/run_ros2_tests.py
    
    Co-authored-by: Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
    
    * Update pycheribuild/projects/cross/ros2.py
    
    Co-authored-by: Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
    
    * set environment with pwd instead of hardcoding source directory
    
    Co-authored-by: Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>

--- pycheribuild/projects/cross/ros2.py
+++ pycheribuild/projects/cross/ros2.py
@@ -25,6 +25,8 @@
 #
 
 import shlex
+import shutil
+import os
 
 from .crosscompileproject import CrossCompileCMakeProject, DefaultInstallDir, GitRepository
 
@@ -37,10 +39,9 @@ class BuildRos2(CrossCompileCMakeProject):
     build_in_source_dir = True
     # it may eventually be useful to install to rootfs or sysroot depending on whether we want to use ROS2
     # as a library for building other applications using cheribuild
-    # therefore, these _install_dir don't do anything, but cheribuild requires them
+    # therefore, the _install_dir doesn't do anything, but cheribuild requires them
     native_install_dir = DefaultInstallDir.IN_BUILD_DIRECTORY
     cross_install_dir = DefaultInstallDir.ROOTFS
-
     dependencies = ["poco"]
     _extra_git_clean_excludes = ["--exclude=src"]  # don't delete src/ when running clean
 
@@ -49,13 +50,14 @@ class BuildRos2(CrossCompileCMakeProject):
         for package in packages:
             cmdline = ["touch", str(self.sourceDir / package / "COLCON_IGNORE")]
             self.run_cmd(cmdline, cwd=self.sourceDir)
-        return
 
     def _run_vcs(self):
+        # this is the meta version control system used by ros for downloading and unpacking repos
         cmdline = ["vcs", "import", "--input", "ros2_minimal.repos", "src"]
-        return self.run_cmd(cmdline, cwd=self.sourceDir)
+        self.run_cmd(cmdline, cwd=self.sourceDir)
 
     def _run_colcon(self, **kwargs):
+        # colcon is the meta build system (on top of cmake) used by ros
         colcon_cmd = ["colcon", "build"]
         colcon_args = ["--no-warn-unused-cli", "--packages-skip-build-finished"]
         cmake_args = ["--cmake-args", "-DBUILD_TESTING=NO"]
@@ -65,10 +67,31 @@ class BuildRos2(CrossCompileCMakeProject):
         if self.config.verbose:
             cmdline.append("--event-handlers")
             cmdline.append("console_cohesion+")
-        return self.run_cmd(cmdline, cwd=self.sourceDir, **kwargs)
+        self.run_cmd(cmdline, cwd=self.sourceDir, **kwargs)
+
+    def _find_file(self, name, path):
+        for root, dirs, files in os.walk(str(path)):
+            if name in files:
+                return os.path.join(root, name)
+        return ""
+
+    def _get_poco(self, **kwargs):
+        # find and copy libPocoFoundation.so.71 from the purecap rootfs into self.sourceDir
+        # this is a bit ugly, but allows us to link the poco library whether we're running
+        # hybrid or purecap cheribsd.  this is helpful because if we're running hybrid cheribsd,
+        # the hybrid rootfs gets mounted, which doesn't include the purecap build of the poco library.
+        #
+        # one day, if we're only running the purecap kernel, we can just append the path
+        # of libPocoFoundation.so.71 to LD_CHERI_LIBRARY_PATH in _set_env() below.
+        libPocoFoundation = self._find_file("libPocoFoundation.so.71", self.rootfs_path)
+        if len(libPocoFoundation) > 0:
+            shutil.copyfile(libPocoFoundation, self.sourceDir / "libPocoFoundation.so.71")
+        else:
+            print("libPocoFoundation.so.71 cannot be found.")
+            print(self.rootfs_path)
 
     def _set_env(self, **kwargs):
-        # create a cheri_setup.csh file in self.sourceDir which can be source'ed
+        # create cheri_setup.csh and cheri_setup.sh files in self.sourceDir which can be source'ed
         # to set environment variables (primarily LD_CHERI_LIBRARY_PATH)
         #
         # based off the install/setup.bash file sourced for ubuntu installs
@@ -78,52 +101,60 @@ class BuildRos2(CrossCompileCMakeProject):
         if not setup_script.is_file():
             print("No setup.bash file to source.")
             return
-        cmdline = shlex.split("bash -c 'source " + str(setup_script) + " | echo $LD_LIBRARY_PATH'")
+        cmdline = shlex.split("bash -c 'source " + str(setup_script) + " && echo $LD_LIBRARY_PATH'")
         output = self.run_cmd(cmdline, cwd=self.sourceDir, captureOutput=True, **kwargs)
 
         # extract LD_LIBRARY_PATH into a variable
-        LD_LIBRARY_PATH = output.stdout.decode("utf-8")
+        LD_LIBRARY_PATH = output.stdout.decode("utf-8").rstrip()
         if len(LD_LIBRARY_PATH) == 0:
             print("LD_LIBRARY_PATH not set.")
             return
 
         # convert LD_LIBRARY_PATH into LD_CHERI_LIBRARY_PATH for CheriBSD
-        LD_LIBRARY_PATHs = LD_LIBRARY_PATH.split(':')
-        LD_CHERI_LIBRARY_PATH = "."
-        for path in LD_LIBRARY_PATHs:
-            LD_CHERI_LIBRARY_PATH += ":" + path
+        LD_LIBRARY_PATH = str(self.sourceDir) + ":" + LD_LIBRARY_PATH
+        LD_LIBRARY_PATH = LD_LIBRARY_PATH.replace(str(self.sourceDir), "`pwd`")
+        LD_CHERI_LIBRARY_PATH = LD_LIBRARY_PATH
+        LD_CHERI_LIBRARY_PATH += ":${LD_CHERI_LIBRARY_PATH}"
+        LD_LIBRARY_PATH += ":${LD_LIBRARY_PATH}"
 
         # write LD_CHERI_LIBRARY_PATH to a text file to source from csh in CheriBSD
         with open(str(self.sourceDir / 'cheri_setup.csh'), 'w') as fout:
             fout.write("#!/bin/csh\n\n")
             fout.write("setenv LD_CHERI_LIBRARY_PATH " + LD_CHERI_LIBRARY_PATH + "\n\n")
-            fout.write("setenv LD_LIBRARY_PATH " + LD_CHERI_LIBRARY_PATH)
-        
-        return
-    
+            fout.write("setenv LD_LIBRARY_PATH " + LD_LIBRARY_PATH)
+
+        # write LD_CHERI_LIBRARY_PATH to a text file to source from sh in CheriBSD
+        with open(str(self.sourceDir / 'cheri_setup.sh'), 'w') as fout:
+            fout.write("#!/bin/sh\n\n")
+            fout.write("export LD_CHERI_LIBRARY_PATH=" + LD_CHERI_LIBRARY_PATH + "\n\n")
+            fout.write("export LD_LIBRARY_PATH=" + LD_LIBRARY_PATH)
+
     def update(self):
         super().update()
         if not (self.sourceDir / "src").is_dir():
             self.makedirs(self.sourceDir / "src")
         self._run_vcs()
         self._ignore_packages()
-        return
 
     def configure(self, **kwargs):
         # overriding this method allows creation of CrossToolchain.cmake
         # without actually calling cmake, as super().configure() would do
         if not self.compiling_for_host():
             self.generate_cmake_toolchain_file(self.sourceDir / "CrossToolchain.cmake")
-        return
 
     def compile(self, **kwargs):
-        return self._run_colcon(**kwargs)
-    
+        self._run_colcon(**kwargs)
+
     def install(self, **kwargs):
         # colcon build performs an install, so we override this to make sure
         # super doesn't attemt to install with ninja
-        #
-        # call the function to create an env setup file
+
+        # call the functions to copy the poco library and create an env setup file
         if not self.compiling_for_host():
-            return self._set_env(**kwargs)
-        return
+            self._get_poco(**kwargs)
+            self._set_env(**kwargs)
+
+    def run_tests(self):
+        # only test when not compiling for host
+        if not self.compiling_for_host():
+            self.run_cheribsd_test_script("run_ros2_tests.py", mount_sourcedir=True, mount_sysroot=True)
---
+++ test-scripts/run_ros2_tests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+#
+# Copyright (c) 2020 Michael Dodson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+import argparse
+
+from run_tests_common import *
+
+
+def run_ros2_tests(qemu: boot_cheribsd.CheriBSDInstance, args: argparse.Namespace) -> bool:
+    boot_cheribsd.info("Running ROS2 tests")
+    boot_cheribsd.set_ld_library_path_with_sysroot(qemu)
+    boot_cheribsd.checked_run_cheribsd_command(qemu, "cd /source && sh -xe ./run-ros2-tests.sh", timeout=240 * 60)
+    return True
+
+if __name__ == '__main__':
+    # we don't need ssh running to execute the tests
+    run_tests_main(test_function=run_ros2_tests, need_ssh=False, should_mount_builddir=False,
+    should_mount_srcdir=True, should_mount_sysroot=True)
