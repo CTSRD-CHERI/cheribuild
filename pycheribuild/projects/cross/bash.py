467dfa9ae7e5e644cad474f5523d88ed87b7a645
Jessica Clarke <jrtc27@jrtc27.com>
2020-06-27 21:21:23 +0100

    bash.py: Add new project for the CHERI port of the GNU Bourne-Again SHell

---
+++ pycheribuild/projects/cross/bash.py
@@ -0,0 +1,44 @@
+#
+# Copyright (c) 2020 Jessica Clarke
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+from .crosscompileproject import CrossCompileAutotoolsProject, DefaultInstallDir, GitRepository
+
+
+class BuildBash(CrossCompileAutotoolsProject):
+    repository = GitRepository("https://github.com/CTSRD-CHERI/bash",
+                               default_branch="cheri")
+    native_install_dir = DefaultInstallDir.IN_BUILD_DIRECTORY
+    cross_install_dir = DefaultInstallDir.ROOTFS
+
+    def setup(self):
+        super().setup()
+        # Bash is horrible K&R C in many places and deliberately uses uses
+        # declarations with no protoype. Hopefully it gets everything right.
+        self.cross_warning_flags.append("-Wno-error=cheri-prototypes")
+
+    def install(self, **kwargs):
+        if self.destdir:
+            self.make_args.set(DESTDIR=self.destdir)
+        super().install(**kwargs)
