ac5d22068f90952cf6b709b3524182146779a9ec
Nathaniel Wesley Filardo <nwf20@cl.cam.ac.uk>
2020-08-10 12:12:20 +0100

    llvm: always build X86 backend
    Some of our tests depend on having this target available.  Most often, it's
    going to be synonymous with "host" and so should produce no extra work.

--- pycheribuild/projects/llvm.py
+++ pycheribuild/projects/llvm.py
@@ -396,7 +396,7 @@ class BuildCheriLLVM(BuildLLVMMonoRepoBase):
         super().setup()
         if not self.build_all_targets:
             # Save some time by only building the targets that we need.
-            self.add_cmake_options(LLVM_TARGETS_TO_BUILD="Mips;RISCV;host")
+            self.add_cmake_options(LLVM_TARGETS_TO_BUILD="Mips;RISCV;X86;host")
 
     def install(self, **kwargs):
         super().install(**kwargs)
