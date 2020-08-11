CTSRD-CHERI/cheribuild

commit 3a6dfea92a55dabdfbce95e804dd5f847bdd0cfd

Fix Jenkins-generated CheriBSD SDK by not stripping .o files
Otherwise we get linker errors due to missing __programe/environ which
are defined in the *crt*.o files.

Also move some code around to share the three copies of ELF file stripping.

pycheribuild/projects/project.py
@@ -773,6 +773,31 @@ class SimpleProject(FileSystemUtils, metaclass=ProjectSubclassDefinitionHook):
                 message += "See " + logfile.name + " for details."
             raise SystemExit(message)
 
+    def maybe_strip_elf_file(self, file: Path, *, output_path: Path = None, print_verbose_only=True) -> bool:
+        """Runs llvm-strip on the file if it is an ELF file and it is safe to do so."""
+        if not file.is_file():
+            return False
+        try:
+            with file.open("rb") as f:
+                if f.read(4) == b"\x7fELF" and self.should_strip_elf_file_for_tarball(file):
+                    self.verbose_print("Stripping ELF binary", file)
+                    cmd = [self.target_info.strip_tool, file]
+                    if output_path:
+                        cmd += ["-o", output_path]
+                    run_command(cmd, print_verbose_only=print_verbose_only)
+                    return True
+        except IOError as e:
+            self.warning("Failed to detect file type for", file, e)
+        return False
+
+    def should_strip_elf_file_for_tarball(self, f: Path):
+        if f.suffix == ".o":
+            # We musn't strip crt1.o, etc. sice if we do the linker can't find essential symbols such as __start
+            # __programe or environ.
+            self.verbose_print("Not stripping", f, "since the symbol table is probably required!")
+            return False
+        return True
+
     def dependency_error(self, *args, install_instructions: str = None):
         self._system_deps_checked = True  # make sure this is always set
         if callable(install_instructions):
@@ -2237,10 +2262,7 @@ add_custom_target(cheribuild-full VERBATIM USES_TERMINAL COMMAND {command} {targ
                     # TODO: make this an error since we should have deleted them
                     self.warning("Will copy a .dump file to the FPGA:", file)
                 # Try to reduce the amount of copied data
-                with file.open("rb") as f:
-                    if f.read(4) == b"\x7fELF":
-                        self.verbose_print("Stripping ELF binary", file)
-                        run_command(self.sdk_bindir / "llvm-strip", file)
+                self.maybe_strip_elf_file(file)
         self.run_cmd("du", "-sh", benchmark_dir)
 
     @property

githooks/pre-push.sh
@@ -101,6 +101,7 @@ do
 		try_run env WORKSPACE=/tmp ./jenkins-cheri-build.py --build --test --tarball -p libcxx-riscv64-purecap
 		try_run env WORKSPACE=/tmp ./jenkins-cheri-build.py --build --cpu=cheri128 -p run-mips-purecap
 		try_run env WORKSPACE=/tmp ./jenkins-cheri-build.py --build --test --tarball --cpu=cheri128 -p llvm-native
+		try_run env WORKSPACE=/tmp ./jenkins-cheri-build.py --build --test --tarball --cpu=cheri128 -p llvm-native --without-sdk
 		try_run env WORKSPACE=/tmp ./jenkins-cheri-build.py --cpu=cheri128 --test run-minimal --keep-install-dir --install-prefix=/rootfs --cheribsd/build-fpga-kernels --no-clean -p
 
 		# Run python tests before pushing

pycheribuild/config/compilation_targets.py
@@ -85,6 +85,10 @@ class _ClangBasedTargetInfo(TargetInfo, metaclass=ABCMeta):
         return self._compiler_dir / "llvm-ar"
 
     @property
+    def strip_tool(self) -> Path:
+        return self._compiler_dir / "llvm-strip"
+
+    @property
     def essential_compiler_and_linker_flags(self) -> typing.List[str]:
         # noinspection PyProtectedMember
         if not self.project._setup_called:

pycheribuild/config/target_info.py
@@ -126,6 +126,11 @@ class TargetInfo(ABC):
 
     @property
     @abstractmethod
+    def strip_tool(self) -> Path:
+        ...
+
+    @property
+    @abstractmethod
     def essential_compiler_and_linker_flags(self) -> typing.List[str]:
         """
         :return: flags such as -target + -mabi which are needed for both compiler and linker
@@ -348,6 +353,10 @@ class NativeTargetInfo(TargetInfo):
     def ar(self) -> Path:
         # Should rarely be needed
         return self.c_compiler.parent / "ar"
+
+    @property
+    def strip_tool(self) -> Path:
+        return self.c_compiler.parent / "strip"
 
     @property
     def c_preprocessor(self) -> Path:

pycheribuild/jenkins.py
@@ -345,14 +345,17 @@ def _jenkins_main():
         status_update("Creating tarball", cheri_config.tarball_name)
         # Strip all ELF files:
         if cheri_config.strip_elf_files:
-            strip_binaries(cheri_config, cheri_config.workspace / "tarball")
+            target = target_manager.get_target_raw(cheri_config.targets[0])
+            Target.instantiating_targets_should_warn = False
+            project = target.get_or_create_project(cheri_config.preferred_xtarget, cheri_config)
+            strip_binaries(cheri_config, project, cheri_config.workspace / "tarball")
         run_command(
             [tar_cmd, "--create", "--xz"] + tar_flags + ["-f", cheri_config.tarball_name, "-C", "tarball", "."],
             cwd=cheri_config.workspace)
         run_command("du", "-sh", cheri_config.workspace / cheri_config.tarball_name)
 
 
-def strip_binaries(cheri_config: JenkinsConfig, directory: Path):
+def strip_binaries(cheri_config: JenkinsConfig, project: SimpleProject, directory: Path):
     status_update("Tarball directory size before stripping ELF files:")
     run_command("du", "-sh", directory)
     for root, dirs, filelist in os.walk(str(directory)):
@@ -361,13 +364,7 @@ def strip_binaries(cheri_config: JenkinsConfig, directory: Path):
             filepath = Path(root, file)
             if filepath.is_symlink():
                 continue
-            try:
-                with filepath.open("rb") as f:
-                    if f.read(4) == b"\x7fELF":
-                        # self.verbose_print("Stripping ELF binary", filepath)
-                        run_command(cheri_config.cheri_sdk_bindir / "llvm-strip", filepath)
-            except Exception as e:
-                warning_message("Failed to detect type of file:", filepath, e)
+            project.maybe_strip_elf_file(filepath)
     status_update("Tarball directory size after stripping ELF files:")
     run_command("du", "-sh", directory)
 

pycheribuild/projects/cross/qt5.py
@@ -578,12 +578,10 @@ class BuildQtWebkit(CrossCompileCMakeProject):
         # create a stripped version of DumpRenderTree and jsc since the one with debug info is too big
         if not self.build_jsc_only:
             dump_render_tree = self.build_dir / "bin/DumpRenderTree"  # type: Path
-            if dump_render_tree.is_file():
-                self.run_cmd(self.llvm_binutils_dir / "llvm-strip", "-o", dump_render_tree.with_suffix(".stripped"),
-                             dump_render_tree)
+            self.maybe_strip_elf_file(dump_render_tree, output_path=dump_render_tree.with_suffix(".stripped"),
+                                      print_verbose_only=False)
         jsc = self.build_dir / "bin/jsc"  # type: Path
-        if jsc.is_file():
-            self.run_cmd(self.llvm_binutils_dir / "llvm-strip", "-o", jsc.with_suffix(".stripped"), jsc)
+        self.maybe_strip_elf_file(jsc, output_path=jsc.with_suffix(".stripped"), print_verbose_only=False)
         self.info("Not installing qtwebit since it uses too much space. If you really want this run `ninja install`")
 
     def run_tests(self):

pycheribuild/projects/disk_image.py
@@ -168,16 +168,12 @@ class _BuildDiskImageBase(SimpleProject):
             path_in_target = os.path.relpath(str(file), str(base_directory))
         assert not str(path_in_target).startswith(".."), path_in_target
 
-        if self.strip_binaries and file.exists():
+        if self.strip_binaries:
             # Try to shrink the size by stripping all elf binaries
-            with file.open("rb") as f:
-                if f.read(4) == b"\x7fELF":
-                    self.verbose_print("Stripping ELF binary", file)
-                    stripped_path = self.tmpdir / path_in_target
-                    self.makedirs(stripped_path.parent)
-                    self.run_cmd(self.sdk_bindir / "llvm-strip", file, "-o", stripped_path, print_verbose_only=True)
-                    # self.run_cmd("file", stripped_path)
-                    file = stripped_path
+            stripped_path = self.tmpdir / path_in_target
+            if self.maybe_strip_elf_file(file, output_path=stripped_path):
+                self.verbose_print("Stripped ELF binary", file)
+                file = stripped_path
 
         if not self.config.quiet:
             self.info(file, " -> /", path_in_target, sep="")

pycheribuild/projects/run_fpga.py
@@ -130,9 +130,9 @@ class LaunchCheriBSDOnFGPA(LaunchFPGABase):
             # Strip to kernel image to save some time when copying it to the FPGA booting
             # TODO: move into beri-fpga-bsd-boot?
             stripped_target = Path(kernel_image_tmpdir, self.current_kernel.name + ".stripped")
-            self.run_cmd(self.config.cheri_sdk_bindir / "llvm-strip", self.current_kernel, "-o", stripped_target)
-            self.run_cmd("du", "-h", self.current_kernel, stripped_target)
-            self.current_kernel = stripped_target
+            if self.maybe_strip_elf_file(self.current_kernel, output_path=stripped_target):
+                self.run_cmd("du", "-h", self.current_kernel, stripped_target)
+                self.current_kernel = stripped_target
             super().process()
 
 # TODO: boot purecap minimal disk image

commit 6b3ca4c303a52c13f4709fd06eaf2e078164f38e

Ensure directory exists when stripping
3a6dfea broke the CheriBSD build. Maybe this is the caller's responsibility
instead so it can batch them, but this is the safest fix.

pycheribuild/projects/project.py
@@ -783,6 +783,7 @@ class SimpleProject(FileSystemUtils, metaclass=ProjectSubclassDefinitionHook):
                     self.verbose_print("Stripping ELF binary", file)
                     cmd = [self.target_info.strip_tool, file]
                     if output_path:
+                        self.makedirs(output_path.parent)
                         cmd += ["-o", output_path]
                     run_command(cmd, print_verbose_only=print_verbose_only)
                     return True