CTSRD-CHERI/cheribuild

commit 9e8b4165f48b8b6ed4a05bdd274fb08fa82add55

samba: Also include Homebrew icu4c on macOS
The build system sucks and doesn't bother including the -I/-L flags in
the pkg-config output when testing that the libraries it wants are
present. What's even the point of using pkg-config in that case...

pycheribuild/projects/samba.py
@@ -31,6 +31,7 @@
 import os
 import shutil
 from pathlib import Path
+from operator import add
 
 from .project import CheriConfig, DefaultInstallDir, GitRepository, MakeCommandKind, Project
 from ..utils import OSInfo, set_env
@@ -115,13 +116,17 @@ class BuildSamba(Project):
 
     def process(self):
         if OSInfo.IS_MAC:
-            # We need readline and krb5 from homebrew:
-            with set_env(PATH="/usr/local/opt/krb5/bin:/usr/local/opt/krb5/sbin:" + os.getenv("PATH", ""),
-                         PKG_CONFIG_PATH="/usr/local/opt/krb5/lib/pkgconfig:/usr/local/opt/readline/lib/pkgconfig:" +
+            # We need icu4c, krb5 and readline from homebrew:
+            homebrew_keg_only_packages = ['icu4c', 'krb5', 'readline']
+            homebrew_dirs = ["/usr/local/opt/" + x for x in homebrew_keg_only_packages]
+            with set_env(PATH=':'.join([x + "/bin" for x in homebrew_dirs]) + ':' +
+                              ':'.join([x + "/sbin" for x in homebrew_dirs]) + ':' +
+                              os.getenv("PATH", ""),
+                         PKG_CONFIG_PATH=':'.join([x + "/lib/pkgconfig" for x in homebrew_dirs]) + ':' +
                                          os.getenv("PKG_CONFIG_PATH", ""),
-                         LDFLAGS="-L/usr/local/opt/krb5/lib -L/usr/local/opt/readline/lib",
-                         CPPFLAGS="-I/usr/local/opt/krb5/include -I/usr/local/opt/readline/include",
-                         CFLAGS="-I/usr/local/opt/krb5/include -I/usr/local/opt/readline/include"):
+                         LDFLAGS=' '.join(["-L" + x + "/lib" for x in homebrew_dirs]),
+                         CPPFLAGS=' '.join(["-I" + x + "/include" for x in homebrew_dirs]),
+                         CFLAGS=' '.join(["-I" + x + "/include" for x in homebrew_dirs])):
                 super().process()
         else:
             super().process()

commit d0112ee929c72a0f89dfc0187d3d593ce2dded5b

Fix unused import that broke CI after 9e8b4165f48b8b6ed4a05bdd274fb08fa82add55

pycheribuild/projects/samba.py
@@ -31,7 +31,6 @@
 import os
 import shutil
 from pathlib import Path
-from operator import add
 
 from .project import CheriConfig, DefaultInstallDir, GitRepository, MakeCommandKind, Project
 from ..utils import OSInfo, set_env