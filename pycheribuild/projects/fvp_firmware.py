41027ed4e0fb1b2a4c62cef0c738a18db4599b97
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-10-23 12:07:32 +0100

    Add gdb-native as a dependency for UEFI to get ld.bfd

--- pycheribuild/projects/fvp_firmware.py
+++ pycheribuild/projects/fvp_firmware.py
@@ -195,6 +195,7 @@ class BuildMorelloUEFI(MorelloFirmwareBase):
     repository = GitRepository("git@git.morello-project.org:morello/edk2.git")
     morello_platforms_repository = GitRepository("git@git.morello-project.org:morello/edk2-platforms.git")
     uefi_tools_repository = GitRepository("https://git.linaro.org/uefi/uefi-tools.git")
+    dependencies = ["gdb-native"]  # To get ld.bfd
     target = "morello-uefi"
     project_name = "morello-edk2"
     default_build_type = BuildType.DEBUG
