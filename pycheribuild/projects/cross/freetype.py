c6c5ceb57ceb551a44a115761d1321d5d295e748
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-12-14 12:45:12 +0000

    Add a target to build freetype
    It compiles fine, but I have not checked whether it works at run time.
    I initially tried using the autotools build system, but that did not
    work without workarounds, whereas using CMake just seems to work.

---
+++ pycheribuild/projects/cross/freetype.py
@@ -0,0 +1,46 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2020 Alex Richardson
+#
+# This work was supported by Innovate UK project 105694, "Digital Security by
+# Design (DSbD) Technology Platform Prototype".
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from .crosscompileproject import CrossCompileCMakeProject, GitRepository
+from ..project import DefaultInstallDir
+
+
+# Prefer the CMake build over autotools since autotools does not work out-of-the-box
+class BuildFreeType2(CrossCompileCMakeProject):
+    project_name = "freetype2"
+    repository = GitRepository("https://github.com/freetype/freetype2.git")
+    cross_install_dir = DefaultInstallDir.SYSROOT_AND_ROOTFS
+    native_install_dir = DefaultInstallDir.DO_NOT_INSTALL
+    path_in_rootfs = "/usr/local"
+
+    def setup(self):
+        super().setup()
+        # FIXME: FT_UINT_TO_POINTER should use uintptr_t
+        self.cross_warning_flags += ["-Wno-error=cheri-capability-misuse"]
+        # TODO: build static instead?
+        self.add_cmake_options(BUILD_SHARED_LIBS=True)
