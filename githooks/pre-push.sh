ee1a1bb6bce4280239f16d3f2bdf8b307c35eff9
Alexander Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-01-11 09:41:10 +0000

    Improve exclusion of known-bad commits in pre-push hook
    Just exclude all commits that are already in origin/master
    instead of using grep -v to exclude two known bad ones.
    
    Co-authored-by: Jessica Clarke <jrtc27@jrtc27.com>

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -48,7 +48,10 @@ check_bad_commit_msg() {
     range=$1
     pattern=$2
     msg=$3
-    bad_commit=$(git rev-list --grep "$pattern" "$range" | grep -v fbb3f6dad35497ed5dcea7e96d0a228b3e383741 | grep -v 4063bcdfe4fcd33b9dd43bf39bd8be3771274ced)
+    # Don't scan commits already in origin/master when creating new branches
+    # for pull requests: At least fbb3f6dad35497ed5dcea7e96d0a228b3e383741 and
+    # 4063bcdfe4fcd33b9dd43bf39bd8be3771274ced have a bad commit message.
+    bad_commit=$(git rev-list --grep "$pattern" "$range" ^origin/master)
     if [ -n "$bad_commit" ]
     then
         echo >&2 "Found $msg commit $bad_commit, not pushing"
