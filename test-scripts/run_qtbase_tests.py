12ef479c43cd8857867c1247573c90a3f469fcd8
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-07-20 18:25:32 +0100

    Fix mounting procfs in the qtbase tests script

--- test-scripts/run_qtbase_tests.py
+++ test-scripts/run_qtbase_tests.py
@@ -55,7 +55,7 @@ def setup_qtbase_tests(qemu: boot_cheribsd.QemuCheriBSDInstance, args: argparse.
     # Possibly similar to https://bugreports.qt.io/browse/QTBUG-87662
     qemu.run("export TZ=Europe/Oslo")
     qemu.checked_run("cd /tmp")
-    qemu.checked_run("mount -t procfs procfs /proc")  # needed for tst_QFile::virtualFile()
+    qemu.checked_run("mkdir -p /proc && mount -t procfs procfs /proc")  # needed for tst_QFile::virtualFile()
     qemu.checked_run("mkdir -p ~/.local/share")  # needed for tst_QFile::moveToTrash()
     if not Path(args.build_dir, "tests/auto/corelib").is_dir():
         # Not running qtbase tests, set LD_LIBRARY_PATH to include QtBase libraries
