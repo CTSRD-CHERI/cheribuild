8b80ab28b2554b6d90b7f136abbb44c8f6cf0756
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-07-26 15:08:14 +0100

    Add QtQuickControls and QtGraphicalEffects modules

--- pycheribuild/projects/cross/qt5.py
+++ pycheribuild/projects/cross/qt5.py
@@ -601,6 +601,31 @@ class BuildQtQuickControls2(BuildQtModuleWithQMake):
         self.run_make()
 
 
+class BuildQtQuickControls(BuildQtModuleWithQMake):
+    target = "qtquickcontrols"
+    dependencies = ["qtdeclarative"]
+    repository = GitRepository("https://code.qt.io/qt/qtquickcontrols.git",
+                               # "https://invent.kde.org/qt/qt/qtquickcontrols.git",
+                               default_branch="5.15", force_branch=True)
+
+    def compile(self, **kwargs):
+        self.run_make()
+
+
+class BuildQtGraphicalEffects(BuildQtModuleWithQMake):
+    target = "qtgraphicaleffects"
+    dependencies = ["qtdeclarative"]
+    # Depends on OpenGL to be useful, I've got a version that allows compiling without OpenGL
+    repository = GitRepository("https://code.qt.io/qt/qtgraphicaleffects.git",
+                               # "https://invent.kde.org/qt/qt/qtgraphicaleffects.git"
+                               temporary_url_override="https://github.com/CTSRD-CHERI/qtgraphicaleffects",
+                               url_override_reason="Includes some workarounds to compile it for -no-opengl",
+                               default_branch="5.15", force_branch=True)
+
+    def compile(self, **kwargs):
+        self.run_make()
+
+
 # Webkit needs ICU (and recommended for QtBase too):
 class BuildICU4C(CrossCompileAutotoolsProject):
     # noinspection PyUnreachableCode
