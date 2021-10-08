e34accccca142f69243be27a124766363210f277
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-10-08 17:53:42 +0100

    Fix drm-kmod build

--- pycheribuild/projects/cross/cheribsd.py
+++ pycheribuild/projects/cross/cheribsd.py
@@ -2201,26 +2201,53 @@ class BuildDrmKMod(CrossCompileProject):
     repository = GitRepository("https://github.com/freebsd/drm-kmod", default_branch="master", force_branch=True)
     supported_architectures = CompilationTargets.ALL_FREEBSD_AND_CHERIBSD_TARGETS
     build_in_source_dir = False
+    use_buildenv = False  # doesn't quite work yet (MAKEOBJDIRPREFIX isn't set)
     freebsd_project: BuildFreeBSD
     kernel_make_args: MakeOptions
 
     def setup(self):
         super().setup()
         self.freebsd_project = self.target_info.get_rootfs_project()
-        extra_make_args = dict(LOCAL_MODULES=self.source_dir.name,
-                           LOCAL_MODULES_DIR=self.source_dir.parent,
-                           MODULES_OVERRIDE="linuxkpi")
+        if self.use_buildenv:
+            extra_make_args = dict(SYSDIR=self.freebsd_project.source_dir / "sys")
+        else:
+            extra_make_args = dict(LOCAL_MODULES=self.source_dir.name,
+                                   LOCAL_MODULES_DIR=self.source_dir.parent,
+                                   MODULES_OVERRIDE="linuxkpi")
         self.kernel_make_args = self.freebsd_project.kernel_make_args_for_config(self.freebsd_project.kernel_config,
                                                                                  extra_make_args)
-        assert self.kernel_make_args.get_var("LOCAL_MODULES") is not None
         assert self.kernel_make_args.kind == MakeCommandKind.BsdMake
 
+    def clean(self, **kwargs):
+        # TODO: use buildenv and only build the kernel modules...
+        if self.use_buildenv:
+            self.info("Cleaning drm-kmod modules for configs:", self.freebsd_project.kernel_config)
+            self.freebsd_project.build_and_install_subdir(self.kernel_make_args, str(self.source_dir),
+                                                          skip_build=True, skip_clean=False, skip_install=True)
+        else:
+            self.info("Clean not supported yet")
+
     def compile(self, **kwargs):
+        # TODO: use buildenv and only build the kernel modules...
         self.info("Building drm-kmod modules for configs:", self.freebsd_project.kernel_config)
-        self.run_make("buildkernel", options=self.kernel_make_args,
-                      cwd=self.freebsd_project.source_dir, parallel=False)
+        if self.use_buildenv:
+            self.freebsd_project.build_and_install_subdir(self.kernel_make_args, str(self.source_dir),
+                                                          skip_build=False, skip_clean=True, skip_install=True)
+        else:
+            self.run_make("buildkernel", options=self.kernel_make_args,
+                          cwd=self.freebsd_project.source_dir, parallel=True)
 
     def install(self, **kwargs):
-        self.info("Building drm-kmod modules for configs:", self.freebsd_project.kernel_config)
-        self.run_make_install(target="installkernel", options=self.kernel_make_args,
-                              cwd=self.freebsd_project.source_dir, parallel=False)
+        # TODO: use buildenv and only install the kernel modules...
+        self.info("Installing drm-kmod modules for configs:", self.freebsd_project.kernel_config)
+        make_args = self.kernel_make_args.copy()
+        # FIXME: it appears that installkernel removes all .ko files, so we can no longer create a disk image
+        # if we install with MODULES_OVERRIDE.
+        make_args.remove_var("MODULES_OVERRIDE")
+        make_args.set_env(METALOG=self.real_install_root_dir / "METALOG.drm-kmod")
+        if self.use_buildenv:
+            self.freebsd_project.build_and_install_subdir(make_args, str(self.source_dir),
+                                                          skip_build=True, skip_clean=True, skip_install=False)
+        else:
+            self.run_make_install(target="installkernel", options=make_args, cwd=self.freebsd_project.source_dir,
+                                  parallel=False)
