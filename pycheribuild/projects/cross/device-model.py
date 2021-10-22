d4a891901c6cdb56cdef5cd7c8693f893d5a22d7
Ruslan Bukin <br@bsdpad.com>
2021-10-22 16:38:04 +0100

    Set compiler and binutils paths.

--- pycheribuild/projects/cross/device-model.py
+++ pycheribuild/projects/cross/device-model.py
@@ -31,6 +31,7 @@
 # SUCH DAMAGE.
 #
 
+from .crosscompileproject import CheriConfig
 from .crosscompileproject import CompilationTargets, CrossCompileAutotoolsProject, DefaultInstallDir, GitRepository
 
 
@@ -49,3 +50,11 @@ class BuildDeviceModel(CrossCompileAutotoolsProject):
     def install(self, **kwargs):
         self.install_file(self.build_dir / "obj/device-model-riscv.bin",
                           self.real_install_root_dir / "device-model-riscv.bin")
+
+    def __init__(self, config: CheriConfig):
+        super().__init__(config)
+        cc = self.config.cheri_sdk_bindir / "clang"
+        objcopy = self.config.cheri_sdk_bindir / "objcopy"
+        self.make_args.env_vars = {"CC": str(cc),
+                                   "AS": str(cc),
+                                   "OBJCOPY": str(objcopy)}
