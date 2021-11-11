bcde96129a017f4b31d5086072f78c879382eee2
Ruslan Bukin <br@bsdpad.com>
2021-11-11 20:25:06 +0000

    Dont include CheriConfig as unused

--- pycheribuild/projects/cross/device-model.py
+++ pycheribuild/projects/cross/device-model.py
@@ -31,7 +31,6 @@
 # SUCH DAMAGE.
 #
 
-from .crosscompileproject import CheriConfig
 from .crosscompileproject import CompilationTargets, CrossCompileAutotoolsProject, DefaultInstallDir, GitRepository
 
 
