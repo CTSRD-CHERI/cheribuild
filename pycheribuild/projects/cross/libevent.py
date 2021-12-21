1bc76a270672ee2cc88166376905565ce23a3197
Jessica Clarke <jrtc27@jrtc27.com>
2021-12-21 00:39:50 +0000

    libevent: New target

---
+++ pycheribuild/projects/cross/libevent.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2021 Jessica Clarke
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+from .crosscompileproject import CrossCompileCMakeProject, GitRepository
+
+
+class BuildLibevent(CrossCompileCMakeProject):
+    repository = GitRepository("https://github.com/libevent/libevent")
+
+    def setup(self):
+        super().setup()
+        self.add_cmake_options(EVENT__DISABLE_MBEDTLS=True)
+        # Couple of harmless "shove integer in void * for generic callback"
+        # instances in the test suite. TODO: Upstream using (u)intptr_t.
+        self.cross_warning_flags.append("-Wno-error=cheri-capability-misuse")
