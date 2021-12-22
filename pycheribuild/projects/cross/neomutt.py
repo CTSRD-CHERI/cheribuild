00b65406ef2bbab4a549eb03b6ec99383f2d12bd
Theo Markettos <git+atm26@cl.cam.ac.uk>
2021-12-22 14:04:35 +0000

    Add a recipe for Neomutt command line email client

---
+++ pycheribuild/projects/cross/neomutt.py
@@ -0,0 +1,47 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright (c) 2021 A. Theodore Markettos
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+from .crosscompileproject import CrossCompileAutotoolsProject
+from .crosscompileproject import CheriConfig, CompilationTargets, CrossCompileProject
+from ..project import GitRepository
+from ...config.compilation_targets import CompilationTargets
+
+
+class BuildNeomutt(CrossCompileAutotoolsProject):
+    repository = GitRepository("https://github.com/neomutt/neomutt.git")
+    supported_architectures = CompilationTargets.ALL_FREEBSD_AND_CHERIBSD_TARGETS + [CompilationTargets.NATIVE]
+    dependencies = ['gettext', 'libxml2']
+
+    def __init__(self, config: CheriConfig):
+        super().__init__(config)
+
+        # neomutt's build system doesn't use autotools, it justs pretends to look the same
+        # - but it doesn't implement the --target option, so we strip it
+        indices = [i for i, s in enumerate(self.configure_args) if '--target=' in s]
+        self.configure_args.pop(indices[0])
+
+        # enable OpenSSL (in base system), disable internationalisation libs we don't have
+        self.configure_args.extend(['--disable-nls', '--disable-idn',
+          '--disable-doc', '--ssl'])
