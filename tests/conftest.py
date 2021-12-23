948969428b8341ab85235bb1c6bdf9cb84678d07
Jessica Clarke <jrtc27@jrtc27.com>
2021-12-23 04:56:07 +0000

    tests: Add conftest.py missing in previous commit

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import argparse
+import pytest
+
+from pycheribuild.config.loader import ConfigLoaderBase, JsonAndCommandLineConfigLoader
+from pycheribuild.config.defaultconfig import DefaultCheriConfig
+from pycheribuild.projects.project import SimpleProject
+from pycheribuild.targets import target_manager
+
+
+class TestArgumentParser(argparse.ArgumentParser):
+    # This is not a test, despite its name matching Test*
+    __test__ = False
+
+    # Don't use sys.exit(), raise an exception instead
+    def exit(self, status=0, message=None):
+        if status == 2:
+            raise KeyError(message)
+        else:
+            raise RuntimeError(status, message)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def _register_targets():
+    loader = JsonAndCommandLineConfigLoader(argparser_class=TestArgumentParser)
+    all_target_names = list(sorted(target_manager.target_names)) + ["__run_everything__"]
+    ConfigLoaderBase._cheri_config = DefaultCheriConfig(loader, all_target_names)
+    ConfigLoaderBase._cheri_config.TEST_MODE = True
+    SimpleProject._config_loader = loader
+    target_manager.register_command_line_options()
+    ConfigLoaderBase._cheri_config.load()
