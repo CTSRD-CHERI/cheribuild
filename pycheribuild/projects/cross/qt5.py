CTSRD-CHERI/cheribuild

commit bc3a9e8c97815a13df7ef04ab25b40287c5cd8c6

Qt5: Depend on libpng, libjpeg and sqlite for native builds
Also add dbus+openGL libraries for non-macOS targets

pycheribuild/projects/cross/qt5.py
@@ -132,12 +132,17 @@ class BuildQtWithConfigureScript(CrossCompileProject):
             # The system X11 libraries might be too old, so add the cheribuild-provided ones as a dependency
             deps.extend(["libx11", "libxcb", "libxkbcommon", "libxcb-cursor", "libxcb-util", "libxcb-image", "libice",
                          "libsm", "libxext", "libxtst", "libxcb-render-util", "libxcb-wm", "libxcb-keysyms"])
+        # Always use our patched image/sql libraries instead of the host ones:
+        deps.extend(["libpng", "libjpeg-turbo", "sqlite"])
+        rootfs_target = cls.get_crosscompile_target(config).get_rootfs_target()
+        deps.append(BuildSharedMimeInfo.get_class_for_target(rootfs_target).target)
         if not cls.get_crosscompile_target(config).is_native():
-            # Assume that we can use system libraries for sqlite+libpng+fonts
-            rootfs_target = cls.get_crosscompile_target(config).get_rootfs_target()
-            deps.extend([BuildSharedMimeInfo.get_class_for_target(rootfs_target).target,
-                         InstallDejaVuFonts.get_class_for_target(rootfs_target).target,
-                         "dbus", "fontconfig", "libpng", "libjpeg-turbo", "sqlite", "libinput"])
+            # We can only depend on fonts when installing to a rootfs, as those need to be installed to a directory
+            # that is only writable by root.
+            deps.extend([InstallDejaVuFonts.get_class_for_target(rootfs_target).target])
+        # For non-macOS we need additional libraries for GUI and openGL parts.
+        if not cls.get_crosscompile_target(config).target_info_cls.is_macos():
+            deps.extend(["dbus", "fontconfig", "libinput"])
             if cls.use_opengl:
                 deps.extend(["libglvnd", "libdrm"])
         return deps

tests/test_argument_parsing.py
@@ -127,17 +127,17 @@ def test_target_subsets(args: "list[str]", expected):
     pytest.param(["--include-dependencies", "--skip-sdk", "--skip-dependency-filter=qtbase.*", "kcoreaddons-amd64"],
                  ["extra-cmake-modules-amd64", "kcoreaddons-amd64"],
                  id="kcoreaddons-amd64-without-qtbase"),
-    pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/no-use-x11",  # skip native X11 deps
+    pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/minimal",  # skip native deps
                   "--skip-dependency-filter=libx.*", "--skip-dependency-filter=xorg.*", "kauth-amd64"],
-                 ["libice-amd64", "libsm-amd64", "shared-mime-info-native", "shared-mime-info-amd64",
-                  "dejavu-fonts-amd64", "libexpat-amd64", "dbus-amd64", "libpng-amd64", "freetype2-amd64",
-                  "fontconfig-amd64", "libjpeg-turbo-amd64", "sqlite-amd64", "linux-input-h-amd64", 'mtdev-amd64',
+                 ["libice-amd64", "libsm-amd64", "libpng-amd64", "libjpeg-turbo-amd64", "sqlite-amd64",
+                  "shared-mime-info-native", "shared-mime-info-amd64", "dejavu-fonts-amd64", "libexpat-amd64",
+                  "dbus-amd64", "freetype2-amd64", "fontconfig-amd64", "linux-input-h-amd64", "mtdev-amd64",
                   "libevdev-amd64", "libudev-devd-amd64", "epoll-shim-amd64", "libinput-amd64", "libglvnd-amd64",
                   "libpciaccess-amd64", "libdrm-amd64", "qtbase-amd64", "extra-cmake-modules-amd64",
                   "kcoreaddons-amd64", "qtbase-native", "extra-cmake-modules-native", "kcoreaddons-native",
                   "kauth-amd64"],
                  id="kauth-amd64-full-without-x11"),
-    pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/no-use-x11",  # skip native X11 deps
+    pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/minimal",  # skip native X11 deps
                   "--skip-dependency-filter=libx.*", "--skip-dependency-filter=xorg.*", "--skip-dependency-filter=qt.*",
                   "kauth-amd64"],
                  ["extra-cmake-modules-amd64", "kcoreaddons-amd64", "extra-cmake-modules-native", "kcoreaddons-native",

tests/test_target_order.py
@@ -202,8 +202,8 @@ def _qtbase_x11_deps(suffix):
                                    "libxcb-", "libxtrans-", "libx11-", "xkeyboard-config-", "libxkbcommon-",
                                    "libxcb-render-util-", "libxcb-util-", "libxcb-image-", "libxcb-cursor-", "libice-",
                                    "libsm-", "libxext-", "libxfixes-", "libxi-", "libxtst-", "libxcb-wm-",
-                                   "libxcb-keysyms-", "shared-mime-info-", "dejavu-fonts-", "libexpat-", "dbus-",
-                                   "libpng-", "freetype2-", "fontconfig-", "libjpeg-turbo-", "sqlite-",
+                                   "libxcb-keysyms-", "libpng-", "libjpeg-turbo-", "sqlite-", "shared-mime-info-",
+                                   "dejavu-fonts-", "libexpat-", "dbus-", "freetype2-", "fontconfig-",
                                    "linux-input-h-", "mtdev-", "libevdev-", "libudev-devd-", "epoll-shim-",
                                    "libinput-", "libglvnd-", "libpciaccess-", "libdrm-")]
     if suffix != "native":
@@ -258,7 +258,8 @@ def test_webkit_cached_deps():
     _check_deps_not_cached([webkit_native])
 
     native_target_names = list(sorted(webkit_native.all_dependency_names(config)))
-    assert native_target_names == ["icu4c-native", "libxml2-native", "qtbase-native", "sqlite-native"]
+    assert native_target_names == ["icu4c-native", "libjpeg-turbo-native", "libpng-native", "libxml2-native",
+                                   "qtbase-native", "shared-mime-info-native", "sqlite-native"]
     _check_deps_cached([webkit_purecap, webkit_riscv, webkit_native])
     assert inspect.getattr_static(webkit_native, "dependencies") == ["qtbase", "icu4c", "libxml2", "sqlite"]
     assert inspect.getattr_static(webkit_purecap, "dependencies") == ["qtbase", "icu4c", "libxml2", "sqlite"]
@@ -270,9 +271,11 @@ def test_webkit_deps_2():
 
     # SDK should not add new targets
     assert _sort_targets(["qtwebkit-native"], add_dependencies=True, skip_sdk=False) == [
-        "qtbase-native", "icu4c-native", "libxml2-native", "sqlite-native", "qtwebkit-native"]
+        "libpng-native", "libjpeg-turbo-native", "sqlite-native", "shared-mime-info-native", "qtbase-native",
+        "icu4c-native", "libxml2-native", "qtwebkit-native"]
     assert _sort_targets(["qtwebkit-native"], add_dependencies=True, skip_sdk=True) == [
-        "qtbase-native", "icu4c-native", "libxml2-native", "sqlite-native", "qtwebkit-native"]
+        "libpng-native", "libjpeg-turbo-native", "sqlite-native", "shared-mime-info-native", "qtbase-native",
+        "icu4c-native", "libxml2-native", "qtwebkit-native"]
 
     assert _sort_targets(["qtwebkit-riscv64"], add_dependencies=True, skip_sdk=True) == _qtbase_x11_deps(
         "riscv64") + ["qtbase-riscv64", "icu4c-native", "icu4c-riscv64", "libxml2-riscv64", "qtwebkit-riscv64"]

commit f46ce481702d0b1e925004767265e8e36e48d7c7

Fix unit tests on non-macOS
Commit bc3a9e8c97815a13df7ef04ab25b40287c5cd8c6 broke the unit tests for
non-macOS systems, but since I ran the pre-commit checks on macOS I did
not notice. Fix this by always using minimal Qt5 dependencies in the unit
tests as those do not differ between operating systems.

pycheribuild/projects/cross/qt5.py
@@ -153,6 +153,9 @@ class BuildQtWithConfigureScript(CrossCompileProject):
     @classmethod
     def dependencies(cls, config: CheriConfig) -> "list[str]":
         deps = super().dependencies(config)
+        rootfs_target = cls.get_crosscompile_target(config).get_rootfs_target()
+        deps.append(BuildSharedMimeInfo.get_class_for_target(rootfs_target).target)
+        deps.append("sqlite")  # TODO: minimal should probably not include QtSql
         if cls.minimal:
             return deps
         if cls.use_x11:
@@ -160,9 +163,7 @@ class BuildQtWithConfigureScript(CrossCompileProject):
             deps.extend(["libx11", "libxcb", "libxkbcommon", "libxcb-cursor", "libxcb-util", "libxcb-image", "libice",
                          "libsm", "libxext", "libxtst", "libxcb-render-util", "libxcb-wm", "libxcb-keysyms"])
         # Always use our patched image/sql libraries instead of the host ones:
-        deps.extend(["libpng", "libjpeg-turbo", "sqlite"])
-        rootfs_target = cls.get_crosscompile_target(config).get_rootfs_target()
-        deps.append(BuildSharedMimeInfo.get_class_for_target(rootfs_target).target)
+        deps.extend(["libpng", "libjpeg-turbo"])
         if not cls.get_crosscompile_target(config).is_native():
             # We can only depend on fonts when installing to a rootfs, as those need to be installed to a directory
             # that is only writable by root.

tests/test_argument_parsing.py
@@ -129,13 +129,13 @@ def test_target_subsets(args: "list[str]", expected):
                  id="kcoreaddons-amd64-without-qtbase"),
     pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/minimal",  # skip native deps
                   "--skip-dependency-filter=libx.*", "--skip-dependency-filter=xorg.*", "kauth-amd64"],
-                 ["libice-amd64", "libsm-amd64", "libpng-amd64", "libjpeg-turbo-amd64", "sqlite-amd64",
-                  "shared-mime-info-native", "shared-mime-info-amd64", "dejavu-fonts-amd64", "libexpat-amd64",
+                 ["shared-mime-info-native", "shared-mime-info-amd64", "sqlite-amd64", "libice-amd64", "libsm-amd64",
+                  "libpng-amd64", "libjpeg-turbo-amd64", "dejavu-fonts-amd64", "libexpat-amd64",
                   "dbus-amd64", "freetype2-amd64", "fontconfig-amd64", "linux-input-h-amd64", "mtdev-amd64",
                   "libevdev-amd64", "libudev-devd-amd64", "epoll-shim-amd64", "libinput-amd64", "libglvnd-amd64",
                   "libpciaccess-amd64", "libdrm-amd64", "qtbase-amd64", "extra-cmake-modules-amd64",
-                  "kcoreaddons-amd64", "qtbase-native", "extra-cmake-modules-native", "kcoreaddons-native",
-                  "kauth-amd64"],
+                  "kcoreaddons-amd64", 'sqlite-native', "qtbase-native", "extra-cmake-modules-native",
+                  "kcoreaddons-native", "kauth-amd64"],
                  id="kauth-amd64-full-without-x11"),
     pytest.param(["--include-dependencies", "--skip-sdk", "--qtbase-native/minimal",  # skip native X11 deps
                   "--skip-dependency-filter=libx.*", "--skip-dependency-filter=xorg.*", "--skip-dependency-filter=qt.*",

tests/test_target_order.py
@@ -198,11 +198,11 @@ def _check_deps_cached(classes):
 
 
 def _qtbase_x11_deps(suffix):
-    result = [x + suffix for x in ("xorg-macros-", "xorgproto-", "xcbproto-", "libxau-", "xorg-pthread-stubs-",
-                                   "libxcb-", "libxtrans-", "libx11-", "xkeyboard-config-", "libxkbcommon-",
-                                   "libxcb-render-util-", "libxcb-util-", "libxcb-image-", "libxcb-cursor-", "libice-",
-                                   "libsm-", "libxext-", "libxfixes-", "libxi-", "libxtst-", "libxcb-wm-",
-                                   "libxcb-keysyms-", "libpng-", "libjpeg-turbo-", "sqlite-", "shared-mime-info-",
+    result = [x + suffix for x in ("shared-mime-info-", "sqlite-", "xorg-macros-", "xorgproto-", "xcbproto-", "libxau-",
+                                   "xorg-pthread-stubs-", "libxcb-", "libxtrans-", "libx11-", "xkeyboard-config-",
+                                   "libxkbcommon-", "libxcb-render-util-", "libxcb-util-", "libxcb-image-",
+                                   "libxcb-cursor-", "libice-", "libsm-", "libxext-", "libxfixes-", "libxi-",
+                                   "libxtst-", "libxcb-wm-", "libxcb-keysyms-", "libpng-", "libjpeg-turbo-",
                                    "dejavu-fonts-", "libexpat-", "dbus-", "freetype2-", "fontconfig-",
                                    "linux-input-h-", "mtdev-", "libevdev-", "libudev-devd-", "epoll-shim-",
                                    "libinput-", "libglvnd-", "libpciaccess-", "libdrm-")]
@@ -219,6 +219,8 @@ def _avoid_native_qtbase_x11_deps():
     qtbase_native = target_manager.get_target_raw("qtbase-native").project_class
     assert issubclass(qtbase_native, BuildQtBase)
     qtbase_native.use_x11 = False
+    qtbase_native.use_opengl = False  # also pulls in some X11 deps right now
+    qtbase_native.minimal = True  # avoid all non-core deps to keep them the same across operating systems
 
 
 def test_ksyntaxhighlighting_includes_native_dependency():
@@ -258,8 +260,8 @@ def test_webkit_cached_deps():
     _check_deps_not_cached([webkit_native])
 
     native_target_names = list(sorted(webkit_native.all_dependency_names(config)))
-    assert native_target_names == ["icu4c-native", "libjpeg-turbo-native", "libpng-native", "libxml2-native",
-                                   "qtbase-native", "shared-mime-info-native", "sqlite-native"]
+    assert native_target_names == ["icu4c-native", "libxml2-native", "qtbase-native", "shared-mime-info-native",
+                                   "sqlite-native"]
     _check_deps_cached([webkit_purecap, webkit_riscv, webkit_native])
     assert inspect.getattr_static(webkit_native, "dependencies") == ["qtbase", "icu4c", "libxml2", "sqlite"]
     assert inspect.getattr_static(webkit_purecap, "dependencies") == ["qtbase", "icu4c", "libxml2", "sqlite"]
@@ -271,11 +273,11 @@ def test_webkit_deps_2():
 
     # SDK should not add new targets
     assert _sort_targets(["qtwebkit-native"], add_dependencies=True, skip_sdk=False) == [
-        "libpng-native", "libjpeg-turbo-native", "sqlite-native", "shared-mime-info-native", "qtbase-native",
-        "icu4c-native", "libxml2-native", "qtwebkit-native"]
+        "shared-mime-info-native", "sqlite-native", "qtbase-native", "icu4c-native", "libxml2-native",
+        "qtwebkit-native"]
     assert _sort_targets(["qtwebkit-native"], add_dependencies=True, skip_sdk=True) == [
-        "libpng-native", "libjpeg-turbo-native", "sqlite-native", "shared-mime-info-native", "qtbase-native",
-        "icu4c-native", "libxml2-native", "qtwebkit-native"]
+        "shared-mime-info-native", "sqlite-native", "qtbase-native", "icu4c-native", "libxml2-native",
+        "qtwebkit-native"]
 
     assert _sort_targets(["qtwebkit-riscv64"], add_dependencies=True, skip_sdk=True) == _qtbase_x11_deps(
         "riscv64") + ["qtbase-riscv64", "icu4c-native", "icu4c-riscv64", "libxml2-riscv64", "qtwebkit-riscv64"]