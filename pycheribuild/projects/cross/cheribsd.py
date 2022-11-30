c03e9efc0d4d8359210d98d5b7498074902e42ab
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2022-11-30 02:58:58 +0000

    cheribsd: caprevoke kernels also in NODEBUG flavours

--- pycheribuild/projects/cross/cheribsd.py
+++ pycheribuild/projects/cross/cheribsd.py
@@ -217,6 +217,7 @@ class RISCVKernelConfigFactory(KernelConfigFactory):
         # Caprevoke kernels
         for kABI in KernelABI:
             configs.append(self.make_config({ConfigPlatform.QEMU}, kABI, caprevoke=True, default=True))
+            configs.append(self.make_config({ConfigPlatform.QEMU}, kABI, caprevoke=True, benchmark=True, default=True))
             configs.append(self.make_config({ConfigPlatform.GFE}, kABI, caprevoke=True, mfsroot=True))
             configs.append(self.make_config({ConfigPlatform.AWS}, kABI, fett=True, caprevoke=True))
 
@@ -253,6 +254,8 @@ class AArch64KernelConfigFactory(KernelConfigFactory):
         for kABI in KernelABI:
             configs.append(self.make_config({ConfigPlatform.QEMU, ConfigPlatform.FVP}, kABI, default=True,
                                             caprevoke=True))
+            configs.append(self.make_config({ConfigPlatform.QEMU, ConfigPlatform.FVP}, kABI, default=True,
+                                            caprevoke=True, benchmark=True))
 
         return configs
 
