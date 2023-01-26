CTSRD-CHERI/cheribuild

commit 1cae9bd31fda8d514e2b4d08b5531222d2cfb4e6

Add a testrig-sail-qemu-rv64 target to test QEMU vs sail-cheri-riscv
This will build and run https://github.com/CTSRD-CHERI/QuickCheckVEngine
to compare QEMU against the reference implementation to detect bugs.

pycheribuild/projects/testrig.py
@@ -1,0 +1,162 @@
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Copyright 2022 Alex Richardson
+# Copyright 2022 Google LLC
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+import subprocess
+import time
+import typing
+from abc import abstractmethod
+
+from .build_qemu import BuildQEMU
+from .project import DefaultInstallDir, MakefileProject, Project
+from .repository import GitRepository
+from .sail import BuildSailCheriRISCV
+from .simple_project import SimpleProject
+from ..processutils import popen, commandline_to_str
+from ..utils import find_free_port
+
+
+# This repository contains various implementations and QuickCheckVEngine
+class BuildTestRig(MakefileProject):
+    do_not_add_to_targets = True
+    repository = GitRepository("https://github.com/CTSRD-CHERI/TestRIG")
+    default_directory_basename = "TestRIG"
+    default_install_dir = DefaultInstallDir.DO_NOT_INSTALL
+    build_in_source_dir = False  # Don't run git clean as part of clean
+
+    def check_system_dependencies(self) -> None:
+        super().check_system_dependencies()
+        self.check_required_system_tool("cabal", apt="cabal-install")
+
+    def clean(self):
+        self.run_make("clean-vengines")
+
+    def compile(self, **kwargs) -> None:
+        self.run_make("vengines")
+
+
+class BuildQuickCheckVengine(Project):
+    target = "quickcheckvengine"
+    repository = GitRepository("https://github.com/CTSRD-CHERI/QuickCheckVEngine")
+    default_directory_basename = "QuickCheckVEngine"
+    default_install_dir = DefaultInstallDir.DO_NOT_INSTALL
+    build_in_source_dir = True
+
+    def check_system_dependencies(self) -> None:
+        super().check_system_dependencies()
+        self.check_required_system_tool("cabal", apt="cabal-install")
+        # TODO: check for at min version?
+        # cabal_version = get_program_version(Path("cabal"), config=self.config, program_name=b"cabal-install")
+
+    def clean(self):
+        self.run_cmd("cabal", "v2-clean", cwd=self.build_dir)
+
+    def compile(self, **kwargs) -> None:
+        self.run_cmd("cabal", "v2-update", cwd=self.build_dir)
+        self.run_cmd("cabal", "v2-build", cwd=self.build_dir)
+
+    def run_qcvengine(self, *args: str, **kwargs):
+        self.run_cmd("cabal", "v2-run", "QCVEngine", "--", *args, cwd=self.build_dir, **kwargs)
+
+    def run_tests(self):
+        self.run_qcvengine("--help", expected_exit_code=1)
+
+
+class RunTestRIG(SimpleProject):
+    do_not_add_to_targets = True
+    number_of_runs: int = 10
+    verification_archstring: "typing.ClassVar[str]"
+    extra_vengine_args: "list[str]" = []
+
+    @abstractmethod
+    def get_reference_implementation_command(self, port: int) -> "list[str]":
+        ...
+
+    @abstractmethod
+    def get_test_implementation_command(self, port: int) -> "list[str]":
+        ...
+
+    @classmethod
+    def setup_config_options(cls, **kwargs) -> None:
+        super().setup_config_options(**kwargs)
+        cls.rerun_last_failure = cls.add_bool_option("rerun-last-failure")
+
+    def process(self) -> None:
+        reference_impl_port = find_free_port()
+        reference_impl_port.socket.close()  # allow sail to use the socket
+        test_impl_port = find_free_port()
+        test_impl_port.socket.close()  # allow QEMU to use the socket
+        with popen(self.get_reference_implementation_command(reference_impl_port.port), config=self.config,
+                   stdin=subprocess.DEVNULL, cwd="/") as reference_cmd:
+            with popen(self.get_test_implementation_command(test_impl_port.port), config=self.config,
+                       stdin=subprocess.DEVNULL, cwd="/") as test_cmd:
+                time.sleep(1)  # wait 1 second for the implementations to start up.
+                if reference_cmd.poll() is not None:
+                    test_cmd.kill()  # kill the other implementation so that the with statement can complete.
+                    self.fatal("Reference implementation failed to start correctly. Command was:",
+                               commandline_to_str(self.get_reference_implementation_command(reference_impl_port.port)))
+                    return
+                elif test_cmd.poll() is not None:
+                    reference_cmd.kill()  # kill the other implementation so that the with statement can complete.
+                    self.fatal("Test implementation failed to start correctly. Command was:",
+                               commandline_to_str(self.get_test_implementation_command(test_impl_port.port)))
+                    return
+                vengine_args = ["-a", str(reference_impl_port.port), "-b", str(test_impl_port.port),
+                                "-r", self.verification_archstring,
+                                "-n", str(self.number_of_runs)]
+                vengine_instance = BuildQuickCheckVengine.get_instance(self)
+                if self.rerun_last_failure:
+                    vengine_args.append("--trace-file=" + str(vengine_instance.build_dir / "last_failure.S"))
+                    # vengine_args.append("--verbose=2")
+                    vengine_args.append("--no-save")
+                else:
+                    vengine_args.append("--verbose=1")
+                BuildQuickCheckVengine.get_instance(self).run_qcvengine(*vengine_args, *self.extra_vengine_args)
+                reference_cmd.kill()
+                test_cmd.kill()
+
+
+class TestRigSailQemuRV64(RunTestRIG):
+    target = "testrig-sail-qemu-rv64"
+    dependencies = ["quickcheckvengine", "sail-cheri-riscv", "qemu"]
+    # NB: can't use GC here since that implicitly enables ihpm in QCVengine and QEMU does not support mcountinhibit
+    # util we have updated to b1675eeb3e6e38b042a23a9647559c9c548c733d.
+    verification_archstring = "rv64imafdc_s_xcheri_zicsr_zifencei"
+    # CClear/FPClear are not implemented in QEMU
+    extra_vengine_args = ["--test-exclude-regex=cclear|fpclear"]
+
+    def get_reference_implementation_command(self, port: int) -> "list[str]":
+        return [str(BuildSailCheriRISCV.get_build_dir(self) / "c_emulator/cheri_riscv_rvfi_RV64"),
+                "--rvfi-dii", str(port),
+                "--enable-misaligned",  # QEMU always enabled misaligned accesses
+                # "--disable-writable-misa",
+                "--trace" if self.rerun_last_failure else "--no-trace"]
+
+    def get_test_implementation_command(self, port: int) -> "list[str]":
+        result = [str(BuildQEMU.get_build_dir(self) / "qemu-system-riscv64cheri"), "--rvfi-dii-port", str(port),
+                  "-cpu", "rv64,g=true,c=true,Counters=true,Zifencei=true,s=true,u=true,Zicsr=true,Xcheri=true",
+                  "-bios", "none"]
+        if self.rerun_last_failure:
+            result.extend(["-d", "instr,int"])
+        return result

tests/test_target_order.py
@@ -418,10 +418,15 @@ def test_no_dependencies_in_build_dir(config: CheriConfig, native_target: Target
     # Ensure that native targets do not depend on other targets that do not install their libraries, etc.
     assert native_target.xtarget.is_native()
     proj = native_target.get_or_create_project(native_target.xtarget, config, caller=None)
-    if isinstance(proj, Project) and proj.get_default_install_dir_kind() in (DefaultInstallDir.IN_BUILD_DIRECTORY,
-                                                                             DefaultInstallDir.DO_NOT_INSTALL):
+    if not isinstance(proj, Project):
+        assert isinstance(proj, SimpleProject)
+        # SimpleProject, so also not installed -> we can ignore this target
+        pytest.skip(f"Skipping {proj.target}")
+        return
+    if proj.get_default_install_dir_kind() in (DefaultInstallDir.IN_BUILD_DIRECTORY, DefaultInstallDir.DO_NOT_INSTALL):
         # Also not installed, we can ignore this target
         pytest.skip(f"Skipping {proj.target}: {proj.get_default_install_dir_kind()}")
+        return
     for dep in proj.all_dependency_names(config):
         dep_project = target_manager.get_target(dep, native_target.xtarget, config, "test").project_class
         assert issubclass(dep_project, SimpleProject)

commit d5d66e317c08a81a91a7a69c0f1dbe0579217a63

testrig: Fix __run_everything__ on macOS
The bluespec-compiler target also checks for cabal, but provides the
Homebrew package name, which is not consistent with these, and thus
fails the consistency assertion for the __checked_system_tools cache.
Fix this by synchronising the checks.

Fixes: 1cae9bd31fda ("Add a testrig-sail-qemu-rv64 target to test QEMU vs sail-cheri-riscv")

pycheribuild/projects/testrig.py
@@ -48,7 +48,7 @@ class BuildTestRig(MakefileProject):
 
     def check_system_dependencies(self) -> None:
         super().check_system_dependencies()
-        self.check_required_system_tool("cabal", apt="cabal-install")
+        self.check_required_system_tool("cabal", apt="cabal-install", homebrew="cabal-install")
 
     def clean(self):
         self.run_make("clean-vengines")
@@ -66,7 +66,7 @@ class BuildQuickCheckVengine(Project):
 
     def check_system_dependencies(self) -> None:
         super().check_system_dependencies()
-        self.check_required_system_tool("cabal", apt="cabal-install")
+        self.check_required_system_tool("cabal", apt="cabal-install", homebrew="cabal-install")
         # TODO: check for at min version?
         # cabal_version = get_program_version(Path("cabal"), config=self.config, program_name=b"cabal-install")
 