CTSRD-CHERI/cheribuild

commit 0e04742a318458205a21960ec130ee959d0e549f

Default to non-purecap builds for LLVM on Morello
It does not compile yet, so use the -native-hybrid variant

pycheribuild/projects/cross/compiler_rt.py
@@ -63,7 +63,8 @@ class BuildCompilerRt(CrossCompileCMakeProject):
         if is_jenkins_build():
             llvm_tools_bindir = self.llvm_project.get_native_install_path(self.config) / "bin"
         else:
-            llvm_tools_bindir = self.llvm_project.get_build_dir(self, cross_target=CompilationTargets.NATIVE) / "bin"
+            llvm_tools_bindir = self.llvm_project.get_build_dir(
+                self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin"
         self.add_cmake_options(
             LLVM_CONFIG_PATH=llvm_tools_bindir / "llvm-config",
             LLVM_EXTERNAL_LIT=llvm_tools_bindir / "llvm-lit",
@@ -159,12 +160,16 @@ class BuildCompilerRtBuiltins(CrossCompileCMakeProject):
         if self.target_info.is_rtems() or self.target_info.is_baremetal():
             self.add_cmake_options(CMAKE_TRY_COMPILE_TARGET_TYPE="STATIC_LIBRARY")  # RTEMS only needs static libs
 
+        if is_jenkins_build() and not self.compiling_for_host():
+            llvm_config_path = self.sdk_bindir / "llvm-config"
+            llvm_lit_path = self.sdk_bindir / "llvm-lit"
+        else:
+            llvm_build_dir = self.llvm_project.get_build_dir(self, cross_target=CompilationTargets.NATIVE_NON_PURECAP)
+            llvm_config_path = llvm_build_dir / "bin/llvm-config",
+            llvm_lit_path = llvm_build_dir / "bin/llvm-lit",
         self.add_cmake_options(
-            LLVM_CONFIG_PATH=self.sdk_bindir / "llvm-config" if is_jenkins_build() and not self.compiling_for_host()
-            else
-            self.llvm_project.get_build_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/llvm-config",
-            LLVM_EXTERNAL_LIT=self.sdk_bindir / "llvm-lit" if is_jenkins_build() and not self.compiling_for_host() else
-            self.llvm_project.get_build_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/llvm-lit",
+            LLVM_CONFIG_PATH=llvm_config_path,
+            LLVM_EXTERNAL_LIT=llvm_lit_path,
             COMPILER_RT_BUILD_BUILTINS=True,
             COMPILER_RT_BUILD_SANITIZERS=False,
             COMPILER_RT_BUILD_XRAY=False,

pycheribuild/config/compilation_targets.py
@@ -84,7 +84,7 @@ class _ClangBasedTargetInfo(TargetInfo, metaclass=ABCMeta):
 
     @classmethod
     def toolchain_targets(cls, target: "CrossCompileTarget", config: "CheriConfig") -> "list[str]":
-        return [cls._get_compiler_project().get_class_for_target(BasicCompilationTargets.NATIVE).target]
+        return [cls._get_compiler_project().get_class_for_target(BasicCompilationTargets.NATIVE_NON_PURECAP).target]
 
     def _rootfs_path(self) -> Path:
         xtarget = self.target.get_rootfs_target()

pycheribuild/projects/cheritrace.py
@@ -28,11 +28,13 @@
 # SUCH DAMAGE.
 #
 from .cmake_project import CMakeProject
+from .cross.llvm import BuildCheriLLVM
 from .project import DefaultInstallDir, GitRepository
 
 
 class BuildCheriTrace(CMakeProject):
     dependencies = ["llvm"]
+    supported_architectures = [BuildCheriLLVM.default_architecture]
     repository = GitRepository("https://github.com/CTSRD-CHERI/cheritrace.git")
     native_install_dir = DefaultInstallDir.CHERI_SDK
 

pycheribuild/projects/cross/libcxx.py
@@ -384,7 +384,8 @@ class _BuildLlvmRuntimes(CrossCompileCMakeProject):
     def dependencies(cls, config: CheriConfig) -> "list[str]":
         if not cls.get_crosscompile_target().is_native():
             return super().dependencies(config)
-        return super().dependencies(config) + [cls.llvm_project.get_class_for_target(CompilationTargets.NATIVE).target]
+        return super().dependencies(config) + [
+            cls.llvm_project.get_class_for_target(CompilationTargets.NATIVE_NON_PURECAP).target]
 
     @classproperty
     def repository(self):
@@ -393,19 +394,22 @@ class _BuildLlvmRuntimes(CrossCompileCMakeProject):
     @property
     def custom_c_preprocessor(self):
         if self.compiling_for_host():
-            return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang-cpp"
+            return self.llvm_project.get_install_dir(
+                self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang-cpp"
         return None
 
     @property
     def custom_c_compiler(self):
         if self.compiling_for_host():
-            return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang"
+            return self.llvm_project.get_install_dir(
+                self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang"
         return None
 
     @property
     def custom_cxx_compiler(self):
         if self.compiling_for_host():
-            return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang++"
+            return self.llvm_project.get_install_dir(
+                self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang++"
         return None
 
     def setup(self):

pycheribuild/projects/cross/llvm.py
@@ -62,6 +62,9 @@ class BuildLLVMBase(CMakeProject):
     is_large_source_repository = True
     # Linking all the debug info takes forever
     default_build_type = BuildType.RELEASE
+    # LLVM does not yet compile for purecap.
+    supported_architectures = [CompilationTargets.NATIVE_NON_PURECAP]
+    default_architecture = CompilationTargets.NATIVE_NON_PURECAP
 
     included_projects: "ClassVar[list[str]]"
     add_default_sysroot: "ClassVar[bool]"
@@ -74,6 +77,13 @@ class BuildLLVMBase(CMakeProject):
     dylib: "ClassVar[bool]"
     install_toolchain_only: "ClassVar[bool]"
     build_minimal_toolchain: "ClassVar[bool]"
+
+    @staticmethod
+    def custom_target_name(base_target: str, xtarget: CrossCompileTarget) -> str:
+        if xtarget is CompilationTargets.NATIVE_NON_PURECAP and xtarget != CompilationTargets.NATIVE:
+            assert xtarget.generic_target_suffix == "native-hybrid", xtarget.generic_target_suffix
+            return base_target + "-native"
+        return base_target + "-" + xtarget.generic_target_suffix
 
     @classmethod
     def is_toolchain_target(cls):
@@ -485,10 +495,10 @@ class BuildCheriLLVM(BuildLLVMMonoRepoBase):
     is_sdk_target = True
     native_install_dir = DefaultInstallDir.CHERI_SDK
     cross_install_dir = DefaultInstallDir.ROOTFS_OPTBASE
-    default_architecture = CompilationTargets.NATIVE
     # NB: remove_duplicates is needed for --enable-hybrid-for-purecap-rootfs targets.
-    supported_architectures = remove_duplicates(CompilationTargets.ALL_SUPPORTED_CHERIBSD_AND_HOST_TARGETS +
-                                                CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS)
+    supported_architectures = remove_duplicates(CompilationTargets.ALL_SUPPORTED_CHERIBSD_TARGETS +
+                                                CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS +
+                                                [CompilationTargets.NATIVE_NON_PURECAP])
     build_all_targets: "ClassVar[bool]"
 
     @classmethod
@@ -551,11 +561,11 @@ class BuildMorelloLLVM(BuildLLVMMonoRepoBase):
     is_sdk_target = True
     native_install_dir = DefaultInstallDir.MORELLO_SDK
     cross_install_dir = DefaultInstallDir.ROOTFS_OPTBASE
-    default_architecture = CompilationTargets.NATIVE
 
     # NB: remove_duplicates is needed for --enable-hybrid-for-purecap-rootfs targets.
-    supported_architectures = remove_duplicates(CompilationTargets.ALL_SUPPORTED_CHERIBSD_AND_HOST_TARGETS +
-                                                CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS)
+    supported_architectures = remove_duplicates(CompilationTargets.ALL_SUPPORTED_CHERIBSD_TARGETS +
+                                                CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS +
+                                                [CompilationTargets.NATIVE_NON_PURECAP])
 
     @property
     def triple_prefixes_for_binaries(self) -> "Iterable[str]":

pycheribuild/projects/cross/llvm_test_suite.py
@@ -51,7 +51,7 @@ class BuildLLVMTestSuiteBase(BenchmarkMixin, CrossCompileCMakeProject):
 
     @classmethod
     def dependencies(cls, config) -> "list[str]":
-        return [cls.llvm_project.get_class_for_target(CompilationTargets.NATIVE).target]
+        return [cls.llvm_project.get_class_for_target(CompilationTargets.NATIVE_NON_PURECAP).target]
 
     # noinspection PyMethodParameters
     @classproperty
@@ -70,7 +70,7 @@ class BuildLLVMTestSuiteBase(BenchmarkMixin, CrossCompileCMakeProject):
                                                 help="Collect statistics from the compiler")
 
     def __find_in_sdk_or_llvm_build_dir(self, name) -> Path:
-        llvm_project = self.llvm_project.get_instance(self, cross_target=CompilationTargets.NATIVE)
+        llvm_project = self.llvm_project.get_instance(self, cross_target=CompilationTargets.NATIVE_NON_PURECAP)
         if (llvm_project.build_dir / "bin" / name).exists():
             return llvm_project.build_dir / "bin" / name
         if is_jenkins_build() and not self.compiling_for_host():
@@ -150,12 +150,14 @@ class BuildLLVMTestSuiteCheriBSDUpstreamLLVM(BuildLLVMTestSuite):
 
     @property
     def custom_c_preprocessor(self):
-        return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang-cpp"
+        return self.llvm_project.get_install_dir(
+            self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang-cpp"
 
     @property
     def custom_c_compiler(self):
-        return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang"
+        return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang"
 
     @property
     def custom_cxx_compiler(self):
-        return self.llvm_project.get_install_dir(self, cross_target=CompilationTargets.NATIVE) / "bin/clang++"
+        return self.llvm_project.get_install_dir(
+            self, cross_target=CompilationTargets.NATIVE_NON_PURECAP) / "bin/clang++"

pycheribuild/projects/cvise.py
@@ -26,6 +26,7 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 from .cmake_project import CMakeProject
+from .cross.llvm import BuildCheriLLVM
 from .project import DefaultInstallDir, GitRepository
 
 
@@ -34,4 +35,5 @@ class BuildCVise(CMakeProject):
     target = "cvise"
     repository = GitRepository("https://github.com/marxin/cvise")
     dependencies = ["llvm"]
+    supported_architectures = [BuildCheriLLVM.default_architecture]
     default_install_dir = DefaultInstallDir.CHERI_SDK

pycheribuild/projects/kdevelop.py
@@ -28,6 +28,7 @@
 # SUCH DAMAGE.
 #
 from .cmake_project import CMakeProject
+from .cross.llvm import BuildCheriLLVM
 from .project import BuildType, DefaultInstallDir, GitRepository
 from .simple_project import SimpleProject
 
@@ -55,6 +56,7 @@ class BuildKDevelop(CMakeProject):
     default_build_type = BuildType.DEBUG
     repository = GitRepository("https://github.com/arichardson/kdevelop.git", default_branch="cheri")
     native_install_dir = DefaultInstallDir.BOOTSTRAP_TOOLS
+    supported_architectures = [BuildCheriLLVM.default_architecture]
 
     def setup(self):
         super().setup()

commit 7c66cd9367767702af49c8ec95ac5be46244ecad

compiler-rt-builtins: Fix invalid type passed to add_cmake_options
The trailing comma meant we were passing a single-element tuple which
made the path invalid when converted to a string.

Fixes: 0e04742a318458205a21960ec130ee959d0e549f

pycheribuild/projects/cross/compiler_rt.py
@@ -165,8 +165,8 @@ class BuildCompilerRtBuiltins(CrossCompileCMakeProject):
             llvm_lit_path = self.sdk_bindir / "llvm-lit"
         else:
             llvm_build_dir = self.llvm_project.get_build_dir(self, cross_target=CompilationTargets.NATIVE_NON_PURECAP)
-            llvm_config_path = llvm_build_dir / "bin/llvm-config",
-            llvm_lit_path = llvm_build_dir / "bin/llvm-lit",
+            llvm_config_path = llvm_build_dir / "bin/llvm-config"
+            llvm_lit_path = llvm_build_dir / "bin/llvm-lit"
         self.add_cmake_options(
             LLVM_CONFIG_PATH=llvm_config_path,
             LLVM_EXTERNAL_LIT=llvm_lit_path,