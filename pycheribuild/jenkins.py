c89a4c17917282d0fb6c7554b99ca3c314284353
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2023-08-10 08:32:07 -0700

    Re-land "Jenkins: change installation directories before calling setup()""
    The underlying issue with not calling setup() should hopefully be fixed.
    
    This reverts commit 2edc5e4592497d5cf19fb450c3eee0f6de3876ff.
    This reverts commit f88311cc8b7a07b6eda59a510afa7f062f0da1ad.

--- pycheribuild/jenkins.py
+++ pycheribuild/jenkins.py
@@ -49,7 +49,6 @@ from .projects import *  # noqa: F401, F403, RUF100
 
 # noinspection PyUnresolvedReferences
 from .projects.cross import *  # noqa: F401, F403, RUF100
-from .projects.cross.crosscompileproject import CrossCompileMixin
 from .projects.project import Project
 from .projects.simple_project import SimpleProject
 from .targets import MultiArchTargetAlias, SimpleTargetAlias, Target, target_manager
@@ -244,46 +243,58 @@ def _jenkins_main() -> None:
         fatal_error("More than one target is not supported yet.", pretend=False)
         sys.exit()
 
-    if JenkinsAction.BUILD in cheri_config.action or JenkinsAction.TEST in cheri_config.action:
-        # Ugly workaround to override all install dirs to go to the tarball
-        for tgt in target_manager.targets(cheri_config):
-            if isinstance(tgt, SimpleTargetAlias):
-                continue
-            cls = tgt.project_class
-            if issubclass(cls, Project) and not isinstance(tgt, MultiArchTargetAlias):
-                cls._default_install_dir_fn = Path(
-                    str(cheri_config.output_root) + str(cheri_config.installation_prefix))
-                i = inspect.getattr_static(cls, "_install_dir")
-                assert isinstance(i, CommandLineConfigOption)
-                # But don't change it if it was specified on the command line. Note: This also does the config
-                # inheritance: i.e. setting --cheribsd/install-dir will also affect cheribsd-cheri/cheribsd-mips
-                # noinspection PyTypeChecker
-                from_cmdline = i.load_option(cheri_config, cls, cls, return_none_if_default=True)
-                if from_cmdline is not None:
-                    status_update("Install directory for", cls.target, "was specified on commandline:", from_cmdline)
-                else:
-                    cls._install_dir = Path(str(cheri_config.output_root) + str(cheri_config.installation_prefix))
-                    cls._check_install_dir_conflict = False
+    expected_install_path = Path(f"{cheri_config.output_root}{cheri_config.installation_prefix}")
+    # Ugly workaround to override all install dirs to go to the tarball
+    for tgt in target_manager.targets(cheri_config):
+        if isinstance(tgt, SimpleTargetAlias):
+            continue
+        cls = tgt.project_class
+        if issubclass(cls, Project) and not isinstance(tgt, MultiArchTargetAlias):
+            cls._default_install_dir_fn = Path(expected_install_path)
+            i = inspect.getattr_static(cls, "_install_dir")
+            assert isinstance(i, CommandLineConfigOption)
+            # But don't change it if it was specified on the command line. Note: This also does the config
+            # inheritance: i.e. setting --cheribsd/install-dir will also affect cheribsd-cheri/cheribsd-mips
+            # noinspection PyTypeChecker
+            from_cmdline = i.load_option(cheri_config, cls, cls, return_none_if_default=True)
+            if from_cmdline is not None:
+                status_update("Install directory for", cls.target, "was specified on commandline:", from_cmdline)
+            else:
+                cls._install_dir = cheri_config.output_root
+                cls._check_install_dir_conflict = False
 
         Target.instantiating_targets_should_warn = False
-        for target in cheri_config.targets:
-            build_target(cheri_config, target_manager.get_target_raw(target))
+        # Override the installation directory for all enabled targets
+        for name in cheri_config.targets:
+            target = target_manager.get_target_raw(name)
+            # noinspection PyProtectedMember
+            project = target._get_or_create_project_no_setup(None, cheri_config, caller=None)
+            if isinstance(project, Project):
+                # Using "/" as the install prefix results inconsistently prefixing some paths with '/usr/'.
+                # To avoid this, just use the full install path as the prefix.
+                if cheri_config.installation_prefix == Path("/"):
+                    project._install_prefix = expected_install_path
+                    project.destdir = Path("/")
+                else:
+                    project._install_prefix = cheri_config.installation_prefix
+                    project.destdir = cheri_config.output_root
+                assert project.real_install_root_dir == expected_install_path
+
+    if JenkinsAction.BUILD in cheri_config.action or JenkinsAction.TEST in cheri_config.action:
+        for tgt in cheri_config.targets:
+            build_target(cheri_config, target_manager.get_target_raw(tgt))
 
     if JenkinsAction.CREATE_TARBALL in cheri_config.action:
         create_tarball(cheri_config)
 
 
 def build_target(cheri_config, target: Target) -> None:
-    # Note: This if exists for now to avoid a large diff.
     target.check_system_deps(cheri_config)
-    # need to set destdir after check_system_deps:
     project = target.get_or_create_project(None, cheri_config, caller=None)
     assert project
     _ = project.all_dependency_names(cheri_config)  # Ensure dependencies are cached.
-    if isinstance(project, CrossCompileMixin):
-        project.destdir = cheri_config.output_root
-        project._install_prefix = cheri_config.installation_prefix
-        project._install_dir = cheri_config.output_root
+    if isinstance(project, Project):
+        assert project.real_install_root_dir == Path(f"{cheri_config.output_root}{cheri_config.installation_prefix}")
 
     if cheri_config.debug_output:
         status_update("Configuration options for building", project.target, file=sys.stderr)
--- tests/test_async_delete.py
+++ tests/test_async_delete.py
@@ -23,17 +23,24 @@ class MockProject(Project):
     def __init__(self, config: MockConfig, name: str):
         self.target = name
         self.default_directory_basename = name
-        expected_src = config.source_root / "sources" / name
-        self._initial_source_dir = expected_src
-        expected_install = config.source_root / "install" / name
-        self._install_dir = expected_install
-        expected_build = Path(config.source_root, "build", name + "-build")
-        self.build_dir = expected_build
+        self.expected_src = config.source_root / "sources" / name
+        self._initial_source_dir = self.expected_src
+        self.expected_install = config.source_root / "install" / name
+        self._install_dir = self.expected_install
+        self.expected_build = Path(config.source_root, "build", name + "-build")
+        self.build_dir = self.expected_build
         super().__init__(config, crosscompile_target=CompilationTargets.NATIVE)
-        assert self.source_dir == expected_src
-        assert self.build_dir == expected_build
-        assert self.install_dir == expected_install
+
+    def setup(self):
+        super().setup()
+        assert self.source_dir == self.expected_src
+        assert self.build_dir == self.expected_build
+        assert self.install_dir == self.expected_install
         self.source_dir.mkdir(parents=True)
+
+    @classmethod
+    def cached_full_dependencies(cls):
+        return []
 
     def _delete_directories(self, *dirs):
         if self.config.sleep_before_delete:
@@ -57,6 +64,7 @@ class TestAsyncDelete(TestCase):
         assert self.tempRoot == self.config.source_root
         assert self.tempRoot / "build" == self.config.build_root
         self.project = MockProject(self.config, "foo")
+        self.project.setup()
         assert self.project.source_dir.exists(), self.project.source_dir
 
     def tearDown(self):
