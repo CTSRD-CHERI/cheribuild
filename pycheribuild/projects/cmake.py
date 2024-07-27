2e3055413f85dbb5df808eec3ded5130e7a651a2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2024-07-27 15:44:48 -0700

    Allow building CMake against newer libuv

--- pycheribuild/projects/cmake.py
+++ pycheribuild/projects/cmake.py
@@ -132,10 +132,12 @@ class BuildCrossCompiledCMake(CMakeProject):
         # Prefer static libraries for 3rd-party dependencies
         self.add_cmake_options(BUILD_SHARED_LIBS=False)
         self.add_cmake_options(CMAKE_USE_SYSTEM_LIBRARY_LIBUV=True)
-        # CMake can't find the static libuv due to a different libname
-        self.add_cmake_options(
-            LibUV_LIBRARY=BuildLibuv.get_install_dir(self) / self.target_info.default_libdir / "libuv_a.a"
-        )
+        if not (BuildLibuv.get_install_dir(self) / self.target_info.default_libdir / "libuv.a").exists():
+            # CMake can't find the static libuv due to a different libname which is installed as libuv_a.a
+            # for some older versions of libuv.
+            self.add_cmake_options(
+                LibUV_LIBRARY=BuildLibuv.get_install_dir(self) / self.target_info.default_libdir / "libuv_a.a"
+            )
 
     def run_tests(self):
         # TODO: generate JUnit output once https://gitlab.kitware.com/cmake/cmake/-/merge_requests/6020 is merged
