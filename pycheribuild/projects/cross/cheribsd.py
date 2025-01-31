b46b9f88cb116e22ca5a7a76bef046ae15e11414
Mark Johnston <markj@FreeBSD.org>
2025-01-31 21:27:59 +0000

    Remove an obsolete hack from the cheribsd buildworld target
    cheribsd commit 2d3d29ceb6a disabled MK_CDDL for purecap riscv due to
    compilation failures, but that has since been resolved.  This hack
    breaks CI now that MK_CDDL is unconditionally enabled in cheribsd (to
    enable dtrace on morello).

--- pycheribuild/projects/cross/cheribsd.py
+++ pycheribuild/projects/cross/cheribsd.py
@@ -46,7 +46,6 @@ from ..project import (
     BuildType,
     CheriConfig,
     ComputedDefaultValue,
-    CPUArchitecture,
     DefaultInstallDir,
     GitRepository,
     MakeCommandKind,
@@ -1100,10 +1099,6 @@ class BuildFreeBSD(BuildFreeBSDBase):
     def buildworld_args(self) -> MakeOptions:
         self._setup_make_args()  # ensure make args are complete
         result = self.make_args.copy()
-        if self.crosscompile_target.is_cheri_hybrid([CPUArchitecture.RISCV64]):
-            # CheriBSD installworld currently gets very confused that libcheri CCDL is forced to false and attempts
-            # to install the files during installworld.
-            result.set_with_options(CDDL=False)
         result.update(self.cross_toolchain_config)
         return result
 
