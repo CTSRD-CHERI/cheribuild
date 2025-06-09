dbafb3f67e6e7611cea70df2342470ec403ca47f
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-06-09 10:31:21 -0700

    Use upstream libxml2 repository
    The incompatibilities have been fixed upstream.

--- pycheribuild/projects/cross/qt5.py
+++ pycheribuild/projects/cross/qt5.py
@@ -1018,7 +1018,9 @@ class BuildICU4C(CrossCompileAutotoolsProject):
 
 # it also needs libxml2
 class BuildLibXml2(CrossCompileCMakeProject):
-    repository = GitRepository("https://github.com/CTSRD-CHERI/libxml2")
+    repository = GitRepository(
+        "https://gitlab.gnome.org/GNOME/libxml2.git", old_urls=[b"https://github.com/CTSRD-CHERI/libxml2"]
+    )
     native_install_dir = DefaultInstallDir.BOOTSTRAP_TOOLS
     supported_architectures = CompilationTargets.ALL_FREEBSD_AND_CHERIBSD_TARGETS + CompilationTargets.ALL_NATIVE
 
