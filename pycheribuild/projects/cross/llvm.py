c7ff3b00995c2dd78af07ecc6fbb3599653346e6
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2025-09-22 13:34:34 +0100

    Linux/llvm: Install bin prefixes with triple

--- pycheribuild/projects/cross/llvm.py
+++ pycheribuild/projects/cross/llvm.py
@@ -42,6 +42,7 @@ from ...config.compilation_targets import (
     CheriBSDTargetInfo,
     CompilationTargets,
     FreeBSDTargetInfo,
+    LinuxMorelloTargetInfo,
 )
 from ...config.target_info import CompilerType, CrossCompileTarget
 from ...processutils import CompilerInfo
@@ -744,6 +745,11 @@ class BuildMorelloLLVM(BuildLLVMMonoRepoBase):
         triples = [
             CheriBSDMorelloTargetInfo.triple_for_target(
                 CompilationTargets.CHERIBSD_MORELLO_PURECAP,
+                self.config,
+                include_version=False,
+            ),
+            LinuxMorelloTargetInfo.triple_for_target(
+                CompilationTargets.LINUX_MORELLO_PURECAP,
                 self.config,
                 include_version=False,
             ),
