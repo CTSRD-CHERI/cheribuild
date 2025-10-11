4d5888a6a1afd4dd327f9f637cf9dd86c2f86eb9
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-10-10 18:07:19 -0700

    Add a ListConfigOption class

--- pycheribuild/projects/simple_project.py
+++ pycheribuild/projects/simple_project.py
@@ -77,6 +77,7 @@ from ..utils import (
 __all__ = [
     "BoolConfigOption",
     "IntConfigOption",
+    "ListConfigOption",
     "PathConfigOption",
     "ReuseOtherProjectBuildDir",
     "SimpleProject",
@@ -184,6 +185,15 @@ if typing.TYPE_CHECKING:
         **kwargs,
     ) -> "Optional[Path]":
         return typing.cast(Optional[Path], default)
+
+    # noinspection PyPep8Naming
+    def ListConfigOption(  # noqa: N802
+        name: str,
+        help: str,
+        default: "typing.Union[Optional[list[T]], ComputedDefaultValue[list[T]]]" = None,
+        **kwargs,
+    ) -> "list[T]":
+        return typing.cast(list[T], default)
 else:
 
     class BoolConfigOption(PerProjectConfigOption):
@@ -238,6 +248,22 @@ else:
             return typing.cast(
                 ConfigOptionHandle,
                 owner.add_config_option(self._name, default=self._default, help=self._help, kind=Path, **self._kwargs),
+            )
+
+    class ListConfigOption(PerProjectConfigOption):
+        def __init__(
+            self,
+            name: str,
+            help: str,
+            default: "typing.Union[Optional[list[T]], ComputedDefaultValue[list[T]]]" = None,
+            **kwargs,
+        ):
+            super().__init__(name, help, default, **kwargs)
+
+        def register_config_option(self, owner: "type[SimpleProject]") -> ConfigOptionHandle:
+            return typing.cast(
+                ConfigOptionHandle,
+                owner.add_list_option(self._name, default=self._default, help=self._help, **self._kwargs),
             )
 
 
