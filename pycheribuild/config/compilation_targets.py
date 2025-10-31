CTSRD-CHERI/cheribuild

commit d47abf081ad5bd606703e5577508992a977db153

Avoid using riscv_cheri_isa in the boot_cheribsd test script
This is not needed if we pass the --bios flag from cheribuild

pycheribuild/config/compilation_targets.py
@@ -81,6 +81,9 @@ class LaunchFreeBSDInterface:
     def get_qemu_mfs_root_kernel(self, use_benchmark_kernel: bool) -> Path:
         raise NotImplementedError()
 
+    def get_riscv_bios_args(self) -> "list[str]":
+        raise NotImplementedError()
+
 
 @functools.lru_cache(maxsize=20)
 def _linker_supports_riscv_relaxations(linker: Path, config: CheriConfig, xtarget: "CrossCompileTarget") -> bool:
@@ -623,6 +626,10 @@ class FreeBSDTargetInfo(_ClangBasedTargetInfo):
                 chosen_qemu = copy.deepcopy(chosen_qemu)  # avoid modifying the object referenced by run_instance
                 chosen_qemu.setup(run_instance)
             cmd.extend(["--qemu-cmd", chosen_qemu.binary])
+            if xtarget.is_riscv() and not has_test_extra_arg_override("--bios"):
+                bios_args = run_instance.get_riscv_bios_args()
+                assert len(bios_args) == 2 and bios_args[0] == "-bios"
+                cmd.extend(["--bios", bios_args[0]])
         if mount_builddir and self.project.build_dir and not has_test_extra_arg_override("--build-dir"):
             cmd.extend(["--build-dir", self.project.build_dir])
         if mount_sourcedir and self.project.source_dir and not has_test_extra_arg_override("--source-dir"):

pycheribuild/boot_cheribsd/__init__.py
@@ -53,10 +53,9 @@ from pathlib import Path
 from typing import Callable, Optional, Union
 
 from ..colour import AnsiColour, coloured
-from ..config.chericonfig import RiscvCheriISA
 from ..config.compilation_targets import CompilationTargets, CrossCompileTarget
 from ..processutils import commandline_to_str, keep_terminal_sane, run_and_kill_children_on_exit
-from ..qemu_utils import QemuOptions, riscv_bios_arguments
+from ..qemu_utils import QemuOptions
 from ..utils import ConfigBase, find_free_port, get_global_config, init_global_config
 
 _cheribuild_root = Path(__file__).parent.parent.parent
@@ -916,8 +915,6 @@ def boot_cheribsd(
             failure("Cannot boot kernel directly and no disk image passed!", exit=True)
     if bios_path is not None:
         bios_args = ["-bios", str(bios_path)]
-    elif qemu_options.xtarget.is_riscv(include_purecap=True):
-        bios_args = riscv_bios_arguments(qemu_options.xtarget, qemu_options.riscv_cheri_isa)
     else:
         bios_args = []
     qemu_args = qemu_options.get_commandline(
@@ -1516,13 +1513,7 @@ def _main(
     args.xtarget = xtarget
     if argparse_adjust_args_callback:
         argparse_adjust_args_callback(args)
-
-    riscv_cheri_isa = None
-    # Slightly ugly hack to avoid having to pass yet another argument for something
-    # that is a temporary workaround until ISAv9 is gone.
-    if args.qemu_cmd is not None and str(args.qemu_cmd).endswith("cheristd"):
-        riscv_cheri_isa = RiscvCheriISA.EXPERIMENTAL_STD093
-    qemu_options = QemuOptions(xtarget, riscv_cheri_isa=riscv_cheri_isa)
+    qemu_options = QemuOptions(xtarget)
     if args.qemu_cmd is not None:
         if not Path(args.qemu_cmd).exists():
             failure("ERROR: Cannot find QEMU binary ", args.qemu_cmd, " doesn't exist", exit=True)

pycheribuild/config/target_info.py
@@ -1063,11 +1063,14 @@ class CrossCompileTarget(typing.Generic[TargetInfoSubclass]):
     def is_riscv(self, include_purecap: Optional[bool] = None) -> bool:
         return self.is_riscv32(include_purecap) or self.is_riscv64(include_purecap)
 
-    def is_experimental_cheri093_std(self, config: "CheriConfig") -> bool:
-        riscv_cheri_isa = self._cheri_isa if self._cheri_isa else config.riscv_cheri_isa
+    def is_experimental_cheri093_std_impl(self, default_isa: "RiscvCheriISA") -> bool:
+        riscv_cheri_isa = self._cheri_isa if self._cheri_isa else default_isa
         return riscv_cheri_isa == RiscvCheriISA.EXPERIMENTAL_STD093 and self.is_hybrid_or_purecap_cheri(
             [CPUArchitecture.RISCV32, CPUArchitecture.RISCV64]
         )
+
+    def is_experimental_cheri093_std(self, config: "CheriConfig") -> bool:
+        return self.is_experimental_cheri093_std_impl(config.riscv_cheri_isa)
 
     def is_arm32(self, include_purecap: Optional[bool] = None) -> bool:
         return self._check_arch(CPUArchitecture.ARM32, include_purecap)

pycheribuild/qemu_utils.py
@@ -248,7 +248,7 @@ def riscv_bios_arguments(
     assert xtarget.is_riscv(include_purecap=True)
     xlen = 32 if xtarget.is_riscv32() else 64
     if xtarget.is_hybrid_or_purecap_cheri([CPUArchitecture.RISCV64]):
-        if cheri_isa == RiscvCheriISA.EXPERIMENTAL_STD093:
+        if xtarget.is_experimental_cheri093_std_impl(cheri_isa):
             # FIXME: QEMU does not yet default to the correct BIOS image name.
             return ["-bios", f"opensbi-riscv{xlen}cheristd-virt-fw_jump.bin"]
         if prefer_bbl:

commit a4196beed125d11afd72b3a3d46d6455fc5659a4

compilation_targets.py: Fix --bios argument to use right index
Fixes: d47abf081ad5 ("Avoid using riscv_cheri_isa in the boot_cheribsd test script")

pycheribuild/config/compilation_targets.py
@@ -629,7 +629,7 @@ class FreeBSDTargetInfo(_ClangBasedTargetInfo):
             if xtarget.is_riscv(include_purecap=True) and not has_test_extra_arg_override("--bios"):
                 bios_args = run_instance.get_riscv_bios_args()
                 assert len(bios_args) == 2 and bios_args[0] == "-bios"
-                cmd.extend(["--bios", bios_args[0]])
+                cmd.extend(["--bios", bios_args[1]])
         if mount_builddir and self.project.build_dir and not has_test_extra_arg_override("--build-dir"):
             cmd.extend(["--build-dir", self.project.build_dir])
         if mount_sourcedir and self.project.source_dir and not has_test_extra_arg_override("--source-dir"):