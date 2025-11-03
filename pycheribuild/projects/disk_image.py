CTSRD-CHERI/cheribuild

commit 6a86e58c13e3753d881d1e378f70f6f978608eda

disk-image-minimal: Include Casper and various services
These are now needed by cheribsdbox due to upstream requiring casper for
syslogd.

pycheribuild/projects/disk_image.py
@@ -1293,6 +1293,16 @@ class BuildMinimalCheriBSDDiskImage(BuildDiskImageBase):
             # Needed for backtrace() (required by CTest)
             "libexecinfo.so.1",  # depends on libelf.so
         ]
+        # casper is needed as of CheriBSD b25a90dd5f4ec60ec9256db3014299059e8013aa
+        optional_libs += [
+            "libcasper.so.1",
+            "libcap_dns.so.2",
+            "libcap_grp.so.1",
+            "libcap_net.so.1",
+            "libcap_netdb.so.1",
+            "libcap_pwd.so.1",
+            "libcap_sysctl.so.2",
+        ]
         # Add the required PAM libraries for su(1)/login(1)
         for i in ("permit", "rootok", "self", "unix", "nologin", "securetty", "lastlog", "login_access"):
             required_libs += ["pam_" + i + ".so", "pam_" + i + ".so.6"]

commit 8b3b32f22a4928af3b3d28da81579cf20861efb1

disk-image-minimal: Include libnv.so.1 for Casper
Fixes: 6a86e58c13e3 ("disk-image-minimal: Include Casper and various services")

pycheribuild/projects/disk_image.py
@@ -1304,6 +1304,7 @@ class BuildMinimalCheriBSDDiskImage(BuildDiskImageBase):
             "libcap_netdb.so.1",
             "libcap_pwd.so.1",
             "libcap_sysctl.so.2",
+            "libnv.so.1",  # used by libcasper in the service interface
         ]
         # Add the required PAM libraries for su(1)/login(1)
         for i in ("permit", "rootok", "self", "unix", "nologin", "securetty", "lastlog", "login_access"):