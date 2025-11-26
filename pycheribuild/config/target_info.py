CTSRD-CHERI/cheribuild

commit d191c0db49a36d368fdceb9648a91e93cfdfc565

Enable -cheri-codeptr-relocs on Morello.
This also adds the -Wl,--local-caprelocs=elf flag.

pycheribuild/config/target_info.py
@@ -1259,6 +1259,9 @@ def cheribsd_morello_version_dependent_flags(cheribsd_version: "Optional[int]", 
     if is_purecap and cheribsd_version is not None and cheribsd_version < 20220511:
         # Use emulated TLS on older purecap
         result.append("-femulated-tls")
+    if cheribsd_version is None or cheribsd_version >= 20250127:
+        result.append("-Wl,--local-caprelocs=elf")
+        result.append("-cheri-codeptr-relocs")
     return result
 
 

commit b400d8923b420e69a2bab7a8e966602d5e00e356

target_info: Fix building gdb-morello-* (and perhaps other targets)
When libiberty's configure script probes for headers like limits.h it
treats any warning as a no (despite not using -Werror), which includes
the unused argument warning for this linker flag. Ideally we would have
separated out compiler and linker flags for this interface, but that
would be a significant undertaking.

Fixes: d191c0db49a3 ("Enable -cheri-codeptr-relocs on Morello.")

pycheribuild/config/target_info.py
@@ -1264,7 +1264,11 @@ def cheribsd_morello_version_dependent_flags(cheribsd_version: "Optional[int]", 
         # Use emulated TLS on older purecap
         result.append("-femulated-tls")
     if cheribsd_version is None or cheribsd_version >= 20250127:
-        result.append("-Wl,--local-caprelocs=elf")
+        # NB: get_essential_compiler_and_linker_flags conflates CFLAGS and
+        # LDFLAGS so this will end up in CFLAGS, but we don't want it to
+        # disrupt configure scripts by warning about the unused argument when
+        # not linking.
+        result.extend(["--start-no-unused-arguments", "-Wl,--local-caprelocs=elf", "--end-no-unused-arguments"])
         result.append("-cheri-codeptr-relocs")
     return result
 