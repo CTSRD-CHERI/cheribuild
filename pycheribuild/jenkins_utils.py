CTSRD-CHERI/cheribuild

commit e4e86b6096adbd4b9d383d8051b103d38b7446b5

Remove cyclic import dependency to improve type checking
This adds a bunch of annoying boilerplate but should
make future cleanups easier. It also seems to have enabled better
pytype analysis so I had to fix/silence some errors.

pycheribuild/jenkins_utils.py
@@ -33,7 +33,7 @@ from typing import Optional
 
 from .config.jenkinsconfig import CheriConfig, JenkinsConfig
 from .config.loader import CommandLineConfigOption
-from .config.target_info import AbstractProject, CrossCompileTarget
+from .config.target_info import CrossCompileTarget
 from .projects.project import Project
 from .targets import MultiArchTargetAlias, SimpleTargetAlias, Target, target_manager
 from .utils import fatal_error, status_update
@@ -67,11 +67,8 @@ def jenkins_override_install_dirs_hack(cheri_config: CheriConfig, install_prefix
     def expected_install_root(tgt: Target) -> Path:
         if tgt in sysroot_targets:
             # noinspection PyProtectedMember
-            proj = tgt._try_get_project()
-            if proj is None:
-                target_info = tgt.xtarget.create_target_info(AbstractProject(cheri_config))
-            else:
-                target_info = proj.target_info
+            proj = tgt._get_or_create_project_no_setup(cross_target=None, config=cheri_config, caller=None)
+            target_info = proj.target_info
             sysroot_dir = target_info.sysroot_dir
             return sysroot_dir
         else:

pycheribuild/config/target_info.py
@@ -31,7 +31,7 @@ import functools
 import platform
 import re
 import typing
-from abc import ABC, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from enum import Enum
 from functools import cached_property
 from pathlib import Path
@@ -160,7 +160,7 @@ _INVALID_INSTALL_DIR: Path = Path("/this/dir/should/be/overwritten/and/not/used/
 _DO_NOT_INSTALL_PATH: Path = Path("/this/project/should/not/be/installed!!!!")
 
 
-class AbstractProject(FileSystemUtils):
+class AbstractProject(FileSystemUtils, metaclass=ABCMeta):
     """A base class for (Simple)Project that exposes only the fields/methods needed in target_info."""
 
     _xtarget: "ClassVar[Optional[CrossCompileTarget]]" = None
@@ -171,16 +171,27 @@ class AbstractProject(FileSystemUtils):
     auto_var_init: AutoVarInit  # Needed for essential_compiler_flags
     config: CheriConfig  # pyrefly: ignore[bad-override]
     crosscompile_target: "CrossCompileTarget"
+    target_info: "TargetInfo"
     target: str
+
+    _setup_called: bool
+    _setup_late_called: bool
+    _system_deps_checked: bool
+    # Old names in the config file (per-architecture) for backwards compat
+    _config_file_aliases: "tuple[str, ...]" = tuple()
 
     # Allow overrides for libc++/llvm-test-suite
     custom_c_preprocessor: Optional[Path] = None
     custom_c_compiler: Optional[Path] = None
     custom_cxx_compiler: Optional[Path] = None
 
-    def __init__(self, config):
+    def __init__(self, config: CheriConfig, *, crosscompile_target: "CrossCompileTarget") -> None:
         super().__init__(config)
+        self.crosscompile_target = crosscompile_target
+        assert self._xtarget == crosscompile_target, "Failed to update all callers?"
         self._init_called = False
+        self._setup_called = False
+        self._setup_late_called = False
 
     def uses_softfloat_by_default(self) -> bool: ...
 
@@ -211,18 +222,74 @@ class AbstractProject(FileSystemUtils):
         assert target is not None
         return target
 
+    def setup(self) -> None:
+        """
+        Class setup that is run just before process()/run_tests/run_benchmarks. This ensures that all dependent targets
+        have been built before and therefore querying e.g. the target compiler will work correctly.
+        """
+        assert not self._setup_called, "Should only be called once"
+        assert not self._setup_late_called, "Should only be called once"
+        self._setup_called = True
+
+    def setup_late(self) -> None:
+        """
+        Like setup(), but called after setup() has been executed for all child classes.
+        This can be used for example when adding configure arguments that depend on state modifications in setup().
+        """
+        assert not self._setup_late_called, "Should only be called once"
+        self._setup_late_called = True
+
     @classmethod
+    @abstractmethod
     def get_instance(
         cls: "type[Type_T]",
         caller: "Optional[AbstractProject]",
         config: "Optional[CheriConfig]" = None,
         cross_target: "Optional[CrossCompileTarget]" = None,
-    ) -> "Type_T":
-        raise NotImplementedError()
+    ) -> "Type_T": ...
 
     @classmethod
     def get_install_dir(cls, caller: "AbstractProject", cross_target: "Optional[CrossCompileTarget]" = None) -> Path:
         raise NotImplementedError()
+
+    @classmethod
+    @abstractmethod
+    def recursive_dependencies(cls, config: CheriConfig) -> list: ...
+
+    @classmethod
+    @abstractmethod
+    def cached_full_dependencies(cls) -> list: ...
+
+    @classmethod
+    @abstractmethod
+    def targets_reset(cls) -> "list[str]": ...
+
+    @classmethod
+    @abstractmethod
+    def all_dependency_names(cls, config: CheriConfig) -> "list[str]": ...
+
+    @classmethod
+    @abstractmethod
+    def _cache_full_dependencies(cls, config, *, allow_already_cached=False) -> None: ...
+
+    # The main API that needs to be implemented:
+    @classmethod
+    @abstractmethod
+    def setup_config_options(cls, **kwargs) -> None: ...
+
+    @abstractmethod
+    def check_system_dependencies(self) -> None: ...
+
+    @abstractmethod
+    def process(self) -> None: ...
+
+    def run_tests(self) -> None:
+        # For the --test option
+        status_update("No tests defined for target", self.target)
+
+    def run_benchmarks(self) -> None:
+        # For the --benchmark option
+        status_update("No benchmarks defined for target", self.target)
 
 
 _AnyProject = typing.TypeVar("_AnyProject", bound=AbstractProject)

pycheribuild/jenkins.py
@@ -354,6 +354,7 @@ def create_tarball(cheri_config) -> None:
         target = target_manager.get_target_raw(cheri_config.targets[0])
         Target.instantiating_targets_should_warn = False
         project = target.get_or_create_project(None, cheri_config, caller=None)
+        assert isinstance(project, SimpleProject)
         project.prepare_install_dir_for_archiving()
         strip_binaries(cheri_config, project, cheri_config.output_root)
     run_command(

pycheribuild/projects/cross/bbl.py
@@ -88,7 +88,7 @@ class BuildBBLBase(CrossCompileAutotoolsProject):
         self.common_warning_flags.append("-Werror=return-type")
         self.common_warning_flags.append("-Wall")
 
-        tinfo = cast(self.target_info, BaremetalClangTargetInfo)
+        tinfo = cast(BaremetalClangTargetInfo, self.target_info)
         # Assume hardfloat architecture and softfloat ABI
         self.configure_args.append(
             "--with-arch=" + tinfo.get_riscv_arch_string(self.crosscompile_target, self.config, softfloat=False),

pycheribuild/projects/cross/llvm.py
@@ -34,7 +34,7 @@ from typing import ClassVar, Iterable, Optional
 
 from ..cmake_project import CMakeProject
 from ..project import BuildType, ComputedDefaultValue, DefaultInstallDir, GitRepository
-from ..simple_project import SimpleProject
+from ..simple_project import SimpleProject, SimpleProjectWithoutDefinitionHook
 from ...config.chericonfig import CheriConfig
 from ...config.compilation_targets import (
     BuildLLVMInterface,
@@ -43,7 +43,7 @@ from ...config.compilation_targets import (
     CompilationTargets,
     FreeBSDTargetInfo,
 )
-from ...config.target_info import AbstractProject, CompilerType, CrossCompileTarget
+from ...config.target_info import CompilerType, CrossCompileTarget
 from ...processutils import CompilerInfo
 from ...utils import InstallInstructions, OSInfo, ThreadJoiner, is_jenkins_build, remove_tuple_duplicates
 
@@ -575,11 +575,14 @@ class BuildLLVMMonoRepoBase(BuildLLVMBase, BuildLLVMInterface):
 
     def add_compiler_with_config_file(self, prefix: str, target: CrossCompileTarget):
         # Create a fake project class that has the required properties needed for essential_compiler_and_linker_flags
-        class MockProject(AbstractProject):
+        class MockProject(SimpleProjectWithoutDefinitionHook):
             def __init__(self, config, _target: CrossCompileTarget):
-                super().__init__(config)
-                self.crosscompile_target = _target
+                self._xtarget = _target
+                super().__init__(config, crosscompile_target=_target)
                 self.needs_sysroot = True
+
+            def process(self):
+                raise NotImplementedError()
 
         prefix += target.build_suffix(self.config, include_os=False)
         # Instantiate the target_info using the mock project:

pycheribuild/targets.py
@@ -49,24 +49,21 @@ from .utils import (
     warning_message,
 )
 
-if typing.TYPE_CHECKING:  # no-combine
-    from .projects.simple_project import SimpleProject  # no-combine
-
 
 class Target:
     instantiating_targets_should_warn: bool = True
 
-    def __init__(self, name, _project_class: "type[SimpleProject]"):
+    def __init__(self, name, _project_class: "type[AbstractProject]"):
         self.name = name
         self._project_class = _project_class
-        self.__project: Optional[SimpleProject] = None
+        self.__project: Optional[AbstractProject] = None
         self._completed = False
         self._tests_have_run = False
         self._benchmarks_have_run = False
         self._creating_project = False  # avoid cycles
 
     @property
-    def project_class(self) -> "type[SimpleProject]":
+    def project_class(self) -> "type[AbstractProject]":
         result = self._project_class
         # noinspection PyProtectedMember
         assert result._xtarget is not None
@@ -80,16 +77,13 @@ class Target:
         return result._xtarget
 
     def get_real_target(
-        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[SimpleProject, str]" = "<unknown>"
+        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[AbstractProject, str]" = "<unknown>"
     ) -> "Target":
         return self
 
-    def _try_get_project(self) -> "Optional[SimpleProject]":
-        return self.__project
-
     def _get_or_create_project_no_setup(
         self, cross_target: Optional[CrossCompileTarget], config, caller: "Optional[AbstractProject]"
-    ) -> "SimpleProject":
+    ) -> "AbstractProject":
         # Note: MultiArchTarget uses cross_target to select the right project (e.g. libcxxrt-native needs
         # libunwind-native path)
         if self.__project is None:
@@ -101,8 +95,8 @@ class Target:
     # noinspection PyProtectedMember
     @final
     def get_or_create_project(
-        self, cross_target: Optional[CrossCompileTarget], config, caller: "Optional[SimpleProject]"
-    ) -> "SimpleProject":
+        self, cross_target: Optional[CrossCompileTarget], config, caller: "Optional[AbstractProject]"
+    ) -> "AbstractProject":
         if caller is not None:
             assert caller._init_called, "Cannot call this inside __init__()"
         project = self._get_or_create_project_no_setup(cross_target, config, caller)
@@ -117,13 +111,13 @@ class Target:
 
     def get_dependencies(self, config: CheriConfig) -> "list[Target]":
         # Due to cyclic imports + forward declarations we need to silence a bad-return-type error here
-        return typing.cast(typing.List[Target], self.project_class.recursive_dependencies(config))
+        return self.project_class.recursive_dependencies(config)
 
     # noinspection PyProtectedMember
     def cache_dependencies(self, config: CheriConfig) -> None:
         self.project_class._cache_full_dependencies(config, allow_already_cached=True)
 
-    def _check_system_deps(self, config: CheriConfig, project: "SimpleProject"):
+    def _check_system_deps(self, config: CheriConfig, project: "AbstractProject"):
         if project._system_deps_checked:
             return
         with set_env(PATH=config.dollar_path_with_other_tools, config=config):
@@ -138,7 +132,7 @@ class Target:
         self._check_system_deps(config, self._get_or_create_project_no_setup(None, config, None))
 
     @final
-    def create_project(self, config: CheriConfig) -> "SimpleProject":
+    def create_project(self, config: CheriConfig) -> "AbstractProject":
         assert not self._creating_project
         if self.instantiating_targets_should_warn:
             raise RuntimeError(coloured(AnsiColour.magenta, "Instantiating target", self.name, "before run()!"))
@@ -147,11 +141,11 @@ class Target:
         result._init_called = True
         return result
 
-    def _create_project(self, config: CheriConfig) -> "SimpleProject":
+    def _create_project(self, config: CheriConfig) -> "AbstractProject":
         return self.project_class(config, crosscompile_target=self.xtarget)
 
     # noinspection PyProtectedMember
-    def _do_run(self, config, msg: str, func: "Callable[[SimpleProject], typing.Any]"):
+    def _do_run(self, config, msg: str, func: "Callable[[AbstractProject], typing.Any]"):
         assert self.__project is not None, "Should have been initialized in check_system_deps()"
         # noinspection PyProtectedMember
         self.cache_dependencies(config)
@@ -232,7 +226,7 @@ class MultiArchTarget(Target):
     def __init__(
         self,
         name,
-        project_class: "type[SimpleProject]",
+        project_class: "type[AbstractProject]",
         target_arch: "CrossCompileTarget",
         base_target: "MultiArchTargetAlias",
     ):
@@ -243,7 +237,7 @@ class MultiArchTarget(Target):
         base_target.derived_targets.append(self)
 
     @property
-    def project_class(self) -> "type[SimpleProject]":
+    def project_class(self) -> "type[AbstractProject]":
         assert self.target_arch is not None
         return self._project_class
 
@@ -252,7 +246,7 @@ class MultiArchTarget(Target):
         assert self.target_arch is not None
         return self.target_arch
 
-    def _create_project(self, config: CheriConfig) -> "SimpleProject":
+    def _create_project(self, config: CheriConfig) -> "AbstractProject":
         return self.project_class(config, crosscompile_target=self.xtarget)
 
     def __repr__(self) -> str:
@@ -261,7 +255,7 @@ class MultiArchTarget(Target):
 
 class _TargetAliasBase(Target, metaclass=abc.ABCMeta):
     @property
-    def project_class(self) -> "type[SimpleProject]":
+    def project_class(self) -> "type[AbstractProject]":
         assert self._project_class is not None
         return self._project_class
 
@@ -269,17 +263,17 @@ class _TargetAliasBase(Target, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def xtarget(self) -> CrossCompileTarget: ...
 
-    def _create_project(self, config: CheriConfig) -> "SimpleProject":
+    def _create_project(self, config: CheriConfig) -> "AbstractProject":
         raise ValueError("Should not be called!")
 
     def get_real_target(
-        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[SimpleProject, str]" = "<unknown>"
+        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[AbstractProject, str]" = "<unknown>"
     ) -> Target:
         raise NotImplementedError()
 
     def _get_or_create_project_no_setup(
         self, cross_target: Optional[CrossCompileTarget], config, caller: "Optional[AbstractProject]"
-    ) -> "SimpleProject":
+    ) -> "AbstractProject":
         if caller is not None:
             # noinspection PyProtectedMember
             assert caller._init_called, "Cannot call this inside __init__()"
@@ -321,7 +315,7 @@ class MultiArchTargetAlias(_TargetAliasBase):
         return cross_target
 
     def get_real_target(
-        self, cross_target: "Optional[CrossCompileTarget]", config, caller: "Union[SimpleProject, str]" = "<unknown>"
+        self, cross_target: "Optional[CrossCompileTarget]", config, caller: "Union[AbstractProject, str]" = "<unknown>"
     ) -> Target:
         assert self.derived_targets, "derived targets must not be empty"
         if cross_target is None:
@@ -358,7 +352,7 @@ class SimpleTargetAlias(_TargetAliasBase):
         return self._real_target.xtarget
 
     def get_real_target(
-        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[SimpleProject, str]" = "<unknown>"
+        self, cross_target: Optional[CrossCompileTarget], config, caller: "Union[AbstractProject, str]" = "<unknown>"
     ) -> Target:
         return self._real_target
 
@@ -371,7 +365,7 @@ class DeprecatedTargetAlias(SimpleTargetAlias):
         self,
         cross_target: Optional[CrossCompileTarget],
         config: "CheriConfig",
-        caller: "Union[SimpleProject, str]" = "<unknown>",
+        caller: "Union[AbstractProject, str]" = "<unknown>",
     ) -> Target:
         warning_message(
             "Using deprecated target ",
@@ -469,7 +463,7 @@ class TargetManager:
         required_arch: Optional[CrossCompileTarget] = None,
         arch_for_unqualified_targets: Optional[CrossCompileTarget] = None,
         config: CheriConfig,
-        caller: "Union[SimpleProject, str]",
+        caller: "Union[AbstractProject, str]",
     ) -> Target:
         target = self.get_target_raw(name)
         # print("get_target", name, arch, end="")
@@ -495,7 +489,6 @@ class TargetManager:
 
         for node in targets:
             for dep in node.project_class.cached_full_dependencies():
-                dep = typing.cast(Target, dep)  # Silence type-checker due to cyclic imports
                 if dep in adj:
                     adj[dep].append(node)
                     in_degree[node] += 1

commit 20e255329d551e36f5db6226698a99f45390359c

jenkins_utils: Don't create projects for sysroot targets too early
Originally this code never created the project, instead creating a dummy
one temporarily to get the target_info, but this was changed to start
creating projects. However, instantiating_targets_should_warn is not yet
False, so that hits a RuntimeError for any target specified in
--sysroot-install-dir-targets.

Instead, wrap it in a ComputedDefaultValue (which is what its type
really should be anyway) so the concrete evaluation of the sysroot_dir
is deferred until we actually create the project later.

Fixes: e4e86b6096ad ("Remove cyclic import dependency to improve type checking")

pycheribuild/jenkins_utils.py
@@ -34,7 +34,7 @@ from typing import Optional
 from .config.jenkinsconfig import CheriConfig, JenkinsConfig
 from .config.loader import CommandLineConfigOption
 from .config.target_info import CrossCompileTarget
-from .projects.project import Project
+from .projects.project import ComputedDefaultValue, Project
 from .targets import MultiArchTargetAlias, SimpleTargetAlias, Target, target_manager
 from .utils import fatal_error, status_update
 
@@ -64,14 +64,18 @@ def jenkins_override_install_dirs_hack(cheri_config: CheriConfig, install_prefix
         if target.xtarget.is_native():
             fatal_error("Cannot use non-existent sysroot for native target", target.name, pretend=False)
 
-    def expected_install_root(tgt: Target) -> Path:
+    def expected_install_root(tgt: Target, as_template=False) -> Path:
         if tgt in sysroot_targets:
+            if as_template:
+                return Path("$SYSROOT")
             # noinspection PyProtectedMember
             proj = tgt._get_or_create_project_no_setup(cross_target=None, config=cheri_config, caller=None)
             target_info = proj.target_info
             sysroot_dir = target_info.sysroot_dir
             return sysroot_dir
         else:
+            if as_template:
+                return Path("$OUTPUT_ROOT")
             return cheri_config.output_root
 
     def expected_install_prefix(tgt: Target) -> Path:
@@ -82,14 +86,17 @@ def jenkins_override_install_dirs_hack(cheri_config: CheriConfig, install_prefix
         else:
             return install_prefix_arg
 
-    def expected_install_path(tgt: Target) -> Path:
-        root_dir = expected_install_root(tgt)
+    def expected_install_path(tgt: Target, as_template=False) -> Path:
+        root_dir = expected_install_root(tgt, as_template)
         install_prefix = expected_install_prefix(tgt)
         return Path(f"{root_dir}{install_prefix}")
 
     for target in all_targets:
         cls = target.project_class
-        cls._default_install_dir_fn = expected_install_path(target)
+        cls._default_install_dir_fn = ComputedDefaultValue(
+            function=lambda config, project: expected_install_path(target),
+            as_string=str(expected_install_path(target, True)),
+        )
 
     Target.instantiating_targets_should_warn = False
     # Now that we have set the _install_dir member, override the prefix/destdir after instantiating.