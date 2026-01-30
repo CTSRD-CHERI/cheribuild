CTSRD-CHERI/cheribuild

commit 7e3f316a31e5265d10fe4b7873181bf8cd973def

Remove the morello-qemu target
The Morello changes were merged into the default CHERI-QEMU branch a long
time ago, so there is no need to have a separate target. If a morello-qemu
installation is found this change will prompt to delete the old files.

pycheribuild/projects/run_qemu.py
@@ -36,7 +36,7 @@ import typing
 from enum import Enum
 from pathlib import Path
 
-from .build_qemu import BuildMorelloQEMU, BuildQEMU, BuildQEMUBase, BuildUpstreamQEMU
+from .build_qemu import BuildQEMU, BuildQEMUBase, BuildUpstreamQEMU
 from .cherios import BuildCheriOS
 from .cross.cheribsd import BuildCHERIBSD, BuildCheriBsdMfsKernel, BuildFreeBSD, ConfigPlatform, KernelABI
 from .cross.freertos import BuildFreeRTOS
@@ -210,25 +210,15 @@ class LaunchQEMUBase(SimpleProject):
         xtarget = cls.get_crosscompile_target(config)
         can_provide_src_via_smb = False
         supported_qemu_classes = []
-        if (xtarget.is_mips(include_purecap=True) or
-                xtarget.is_riscv(include_purecap=True)):
-            can_provide_src_via_smb = True
-            supported_qemu_classes += [BuildQEMU]
-            if not xtarget.is_hybrid_or_purecap_cheri():
-                supported_qemu_classes += [BuildUpstreamQEMU, None]
+        if xtarget.is_mips(include_purecap=True) or xtarget.is_riscv(include_purecap=True):
             can_provide_src_via_smb = True
             supported_qemu_classes += [BuildQEMU]
             if not xtarget.is_hybrid_or_purecap_cheri():
                 supported_qemu_classes += [BuildUpstreamQEMU, None]
         elif xtarget.is_aarch64(include_purecap=True):
             can_provide_src_via_smb = True
-            # Default to Morello QEMU for Morello, since CHERI QEMU builds may
-            # not be up to date, but prefer CHERI QEMU for AArch64 like other
-            # architectures.
-            if xtarget.is_hybrid_or_purecap_cheri():
-                supported_qemu_classes += [BuildMorelloQEMU, BuildQEMU]
-            else:
-                supported_qemu_classes += [BuildQEMU, BuildMorelloQEMU]
+            # Prefer CHERI QEMU for AArch64 like other architectures.
+            supported_qemu_classes += [BuildQEMU]
             if not xtarget.is_hybrid_or_purecap_cheri():
                 supported_qemu_classes += [BuildUpstreamQEMU, None]
         elif xtarget.is_any_x86() or xtarget.is_aarch64(include_purecap=False):
@@ -257,7 +247,6 @@ class LaunchQEMUBase(SimpleProject):
             elif cls.use_qemu in (QEMUType.CHERI, QEMUType.MORELLO, QEMUType.UPSTREAM):
                 qemu_class = {
                     QEMUType.CHERI: BuildQEMU,
-                    QEMUType.MORELLO: BuildMorelloQEMU,
                     QEMUType.UPSTREAM: BuildUpstreamQEMU
                 }[cls.use_qemu]
                 if qemu_class not in supported_qemu_classes:
@@ -282,7 +271,7 @@ class LaunchQEMUBase(SimpleProject):
                     qemu_binary = None
                 else:
                     # Only CHERI QEMU supports more than one SMB share
-                    can_provide_src_via_smb = qemu_class in [BuildQEMU, BuildMorelloQEMU]
+                    can_provide_src_via_smb = qemu_class == BuildQEMU
                     qemu_binary = qemu_class.qemu_binary(cls, xtarget=xtarget, config=config)
 
         cls._cached_chosen_qemu = ChosenQEMU(qemu_class, qemu_binary, can_provide_src_via_smb)

pycheribuild/config/chericonfig.py
@@ -520,10 +520,6 @@ class CheriConfig(ConfigBase):
         return self.cheri_sdk_bindir
 
     @property
-    def morello_qemu_bindir(self):
-        return self.morello_sdk_bindir
-
-    @property
     def test_ssh_key(self) -> Path:
         if self._test_ssh_key is not None:
             return self._test_ssh_key

pycheribuild/config/jenkinsconfig.py
@@ -209,23 +209,6 @@ class JenkinsConfig(CheriConfig):
             os_suffix = "unknown-os"
         return self.workspace / ("qemu-" + os_suffix) / "bin"
 
-    @property
-    def morello_qemu_bindir(self):
-        for i in self.morello_sdk_bindir.glob("qemu-system-*"):
-            if self.verbose:
-                print("Found QEMU binary", i, "in Morello SDK dir -> using that for Morello QEMU binaries")
-            # If one qemu-system-foo exists in the morello_sdk_bindir use that instead of $WORKSPACE/qemu-<OS>
-            return self.morello_sdk_bindir
-        if OSInfo.IS_LINUX:
-            os_suffix = "linux"
-        elif OSInfo.IS_FREEBSD:
-            os_suffix = "freebsd"
-        elif OSInfo.IS_MAC:
-            os_suffix = "mac"
-        else:
-            os_suffix = "unknown-os"
-        return self.workspace / ("qemu-" + os_suffix) / "bin"
-
     def load(self):
         super().load()
 

pycheribuild/projects/build_qemu.py
@@ -347,29 +347,16 @@ class BuildQEMU(BuildQEMUBase):
                     tgt_info_riscv64.get_essential_compiler_and_linker_flags()).replace("=", " ")
             ])
 
-
-class BuildMorelloQEMU(BuildQEMU):
-    repository = GitRepository("https://github.com/CTSRD-CHERI/qemu.git", default_branch="qemu-morello-merged",
-                               force_branch=True,
-                               old_urls=[
-                                   b"https://github.com/LawrenceEsswood/qemu.git",
-                                   # None of these were provided by cheribuild, but try and handle common
-                                   # insteadOf/pushInsteadOf configs that will otherwise confuse cheribuild as they
-                                   # affect the output of git remote.
-                                   b"ssh://git@github.com/LawrenceEsswood/qemu.git",
-                                   b"ssh://github.com/LawrenceEsswood/qemu.git",
-                                   b"git@github.com:LawrenceEsswood/qemu.git"
-                               ])
-    native_install_dir = DefaultInstallDir.MORELLO_SDK
-    default_targets = "aarch64-softmmu,morello-softmmu"
-    target = "morello-qemu"
-    hide_options_from_help = True
-
-    @classmethod
-    def qemu_binary_for_target(cls, xtarget: CrossCompileTarget, config: CheriConfig):
-        if xtarget.is_aarch64(include_purecap=True):
-            # Always use the Morello qemu even for plain AArch64:
-            binary_name = "qemu-system-morello"
-        else:
-            raise ValueError("Invalid xtarget" + str(xtarget))
-        return config.morello_qemu_bindir / os.getenv("QEMU_MORELLO_PATH", binary_name)
+    def install(self, **kwargs):
+        super().install(**kwargs)
+        # Delete the old Morello-QEMU files
+        self._cleanup_old_files(self.config.morello_sdk_dir / "share/qemu",
+                                self.config.morello_sdk_dir / "share/applications/qemu.desktop",
+                                self.config.morello_sdk_dir / "libexec/qemu-bridge-helper",
+                                self.config.morello_sdk_dir / "libexec/virtfs-proxy-helper",
+                                self.config.morello_sdk_dir / "bin/elf2dmp",
+                                self.config.morello_sdk_dir / "bin/symbolize-cheri-trace.py",
+                                *(self.config.morello_sdk_dir / "bin").glob("qemu-*"),
+                                *self.config.morello_sdk_dir.rglob("share/icons/**/qemu.png"),
+                                *self.config.morello_sdk_dir.rglob("share/icons/**/qemu.bmp"),
+                                *self.config.morello_sdk_dir.rglob("share/icons/**/qemu.svg"))

pycheribuild/projects/project.py
@@ -1033,7 +1033,19 @@ class SimpleProject(FileSystemUtils, metaclass=ProjectSubclassDefinitionHook):
             return False
         return True
 
-    def _cleanup_old_files(self, current_path: Path, current_suffix: str, old_suffixes: typing.List[str]):
+    def _cleanup_old_files(self, *old_paths: Path, default_delete=True):
+        for p in old_paths:
+            if not p.exists():
+                continue
+            filetype = "directory" if p.is_dir() else "file"
+            self.warning(f"Found old {filetype} {p} that is now obsolete.")
+            if self.query_yes_no(f"Would you like to remove the old {filetype} {p}", default_result=default_delete):
+                if p.is_dir():
+                    self._delete_directories(p)
+                else:
+                    self.delete_file(p)
+
+    def _cleanup_renamed_files(self, current_path: Path, current_suffix: str, old_suffixes: typing.List[str]):
         """Remove old build directories/disk-images, etc. to avoid wasted disk space after renaming targets"""
         if not old_suffixes:
             return

pycheribuild/projects/sdk.py
@@ -102,7 +102,7 @@ class BuildFreestandingSdk(SimpleProject):
 
 class BuildFreestandingMorelloSdk(TargetAliasWithDependencies):
     target = "freestanding-morello-sdk"
-    dependencies = ["morello-llvm-native", "morello-qemu"]  # "morello-gdb-native" does not exist
+    dependencies = ["morello-llvm-native", "qemu"]  # "morello-gdb-native" does not exist
     dependencies_must_be_built = True
     is_sdk_target = True
 

tests/test_target_order.py
@@ -62,7 +62,7 @@ cheribsd_sdk_deps = freestanding_deps + ["cheribsd-riscv64-hybrid", "cheribsd-sd
     # Ensure that cheribsd is added to deps even on Linux/Mac
     pytest.param("cheribsd-sdk-riscv64-hybrid", cheribsd_sdk_deps, id="cheribsd-sdk"),
     pytest.param("sdk-riscv64-hybrid", cheribsd_sdk_deps + ["sdk-riscv64-hybrid"], id="sdk"),
-    pytest.param("sdk-morello-purecap", ["morello-llvm-native", "morello-qemu", "freestanding-morello-sdk",
+    pytest.param("sdk-morello-purecap", ["morello-llvm-native", "qemu", "freestanding-morello-sdk",
                                          "cheribsd-morello-purecap", "cheribsd-sdk-morello-purecap",
                                          "sdk-morello-purecap"], id="morello-purecap"),
 ])
@@ -131,10 +131,10 @@ def test_build_and_run(target_name, expected_list):
     # Note: For architectures that CHERI QEMU builds by default we currently
     # explicitly default to using that rather than the system QEMU.
     pytest.param("run-morello-hybrid", True,
-                 ["morello-qemu", "morello-llvm-native", "cheribsd-morello-hybrid", "gdb-morello-hybrid",
+                 ["qemu", "morello-llvm-native", "cheribsd-morello-hybrid", "gdb-morello-hybrid",
                   "disk-image-morello-hybrid"]),
     pytest.param("run-morello-purecap", True,
-                 ["morello-qemu", "morello-llvm-native", "cheribsd-morello-purecap",
+                 ["qemu", "morello-llvm-native", "cheribsd-morello-purecap",
                   "gdb-morello-hybrid-for-purecap-rootfs", "disk-image-morello-purecap"]),
     pytest.param("run-riscv64", True,
                  ["qemu", "llvm-native", "cheribsd-riscv64", "gdb-riscv64", "disk-image-riscv64"]),

commit d8e86fc9b44a8b3177a0b0681da21f6bd191f14c

run-*: Remove --<target>/use-qemu=morello option
Since morello-qemu was removed in favour of just using qemu (CHERI QEMU)
this has been broken, since the entry in the dictionary used for
qemu_class was removed but we still tried to look it up, giving a
KeyError.

Fixes: 7e3f316a31e5 ("Remove the morello-qemu target")

pycheribuild/projects/run_qemu.py
@@ -66,7 +66,6 @@ def get_default_ssh_forwarding_port(addend: int):
 class QEMUType(Enum):
     DEFAULT = "default"
     CHERI = "cheri"
-    MORELLO = "morello"
     UPSTREAM = "upstream"
     SYSTEM = "system"
     CUSTOM = "custom"
@@ -324,7 +323,7 @@ class LaunchQEMUBase(SimpleProject):
             if cls.use_qemu == QEMUType.DEFAULT:
                 qemu_class = supported_qemu_classes[0]
                 qemu_binary = None
-            elif cls.use_qemu in (QEMUType.CHERI, QEMUType.MORELLO, QEMUType.UPSTREAM):
+            elif cls.use_qemu in (QEMUType.CHERI, QEMUType.UPSTREAM):
                 qemu_class = {
                     QEMUType.CHERI: BuildQEMU,
                     QEMUType.UPSTREAM: BuildUpstreamQEMU,