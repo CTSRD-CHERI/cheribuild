CTSRD-CHERI/cheribuild

commit 076888d3a5c8acc557a656dd2f1015dbbe31aaa7

gdb: Support building CHERI GDB as purecap and switch default ABI to purecap
Upstream GDB remains hybrid as does the CHERI Alliance GDB.

pycheribuild/projects/cross/gdb.py
@@ -57,13 +57,12 @@ class BuildGDBBase(CrossCompileAutotoolsProject):
     is_sdk_target = True
     default_build_type = BuildType.RELEASE
     _supported_architectures = (
-        *CompilationTargets.ALL_CHERIBSD_NON_CHERI_TARGETS,
-        *CompilationTargets.ALL_CHERIBSD_HYBRID_TARGETS,
-        *CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS,
-        *CompilationTargets.ALL_SUPPORTED_FREEBSD_TARGETS,
-        CompilationTargets.NATIVE_NON_PURECAP,
+        CompilationTargets.ALL_CHERIBSD_TARGETS_WITH_HYBRID
+        + CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS
+        + CompilationTargets.ALL_SUPPORTED_FREEBSD_TARGETS
+        + CompilationTargets.ALL_NATIVE
     )
-    _default_architecture = CompilationTargets.NATIVE_NON_PURECAP
+    _default_architecture = CompilationTargets.NATIVE
     prefer_full_lto_over_thin_lto = True
 
     @staticmethod
@@ -275,6 +274,15 @@ class BuildUpstreamGDB(BuildGDBBase):
         inherit=BuildGDBBase._default_install_dir_fn,
     )
 
+    _supported_architectures = (
+        *CompilationTargets.ALL_CHERIBSD_NON_CHERI_TARGETS,
+        *CompilationTargets.ALL_CHERIBSD_HYBRID_TARGETS,
+        *CompilationTargets.ALL_CHERIBSD_HYBRID_FOR_PURECAP_ROOTFS_TARGETS,
+        *CompilationTargets.ALL_SUPPORTED_FREEBSD_TARGETS,
+        CompilationTargets.NATIVE_NON_PURECAP,
+    )
+    _default_architecture = CompilationTargets.NATIVE_NON_PURECAP
+
 
 class BuildGDB(BuildGDBBase):
     path_in_rootfs = "/usr/local"  # Always install gdb as /usr/local/bin/gdb
@@ -308,6 +316,7 @@ class BuildCheriAllianceGDB(BuildGDBBase):
         CompilationTargets.CHERIBSD_RISCV_HYBRID_FOR_PURECAP_ROOTFS,
         CompilationTargets.NATIVE_NON_PURECAP,
     )
+    _default_architecture = CompilationTargets.NATIVE_NON_PURECAP
 
 
 class BuildKGDB(BuildGDB):
@@ -324,10 +333,20 @@ class BuildKGDB(BuildGDB):
     )
 
 
+def get_gdb_xtarget(target: CrossCompileTarget, config: CheriConfig) -> CrossCompileTarget:
+    if target.is_cheri_purecap() and target.is_experimental_cheri093_std(config):
+        return target.get_cheri_hybrid_for_purecap_rootfs_target()
+    return target
+
+
 def get_build_gdb_class(target: CrossCompileTarget, config: CheriConfig) -> "type[BuildGDBBase]":
     return BuildCheriAllianceGDB if target.is_experimental_cheri093_std(config) else BuildGDB
 
 
 def get_native_gdb_binary_to_debug_target(target: CrossCompileTarget, caller: AbstractProject) -> Path:
     real_cls = get_build_gdb_class(target, caller.config)
-    return real_cls.get_install_dir(caller, CompilationTargets.NATIVE_NON_PURECAP) / "bin/gdb"
+    if target.is_experimental_cheri093_std(caller.config):
+        prefix = CompilationTargets.NATIVE_NON_PURECAP
+    else:
+        prefix = CompilationTargets.NATIVE
+    return real_cls.get_install_dir(caller, prefix) / "bin/gdb"

pycheribuild/projects/disk_image.py
@@ -39,7 +39,7 @@ from pathlib import Path, PurePath
 from typing import Optional, Union
 
 from .cross.cheribsd import BuildCHERIBSD, BuildFreeBSD, BuildFreeBSDWithDefaultOptions
-from .cross.gdb import BuildKGDB, get_build_gdb_class
+from .cross.gdb import BuildKGDB, get_build_gdb_class, get_gdb_xtarget
 from .project import (
     AutotoolsProject,
     CheriConfig,
@@ -570,8 +570,7 @@ class BuildDiskImageBase(SimpleProject):
             return
         # FIXME: if /usr/local/bin/gdb is in the image make /usr/bin/gdb a symlink
         cross_target = self.source_project.crosscompile_target
-        if cross_target.is_cheri_purecap():
-            cross_target = cross_target.get_cheri_hybrid_for_purecap_rootfs_target()
+        cross_target = get_gdb_xtarget(cross_target, self.config)
         gdb_cls = get_build_gdb_class(cross_target, self.config)
         if cross_target not in gdb_cls.supported_architectures():
             self.warning("GDB cannot be built for architecture ", cross_target, " -> not addding it")
@@ -1454,7 +1453,7 @@ class BuildCheriBSDDiskImage(BuildDiskImageBase):
         result = super().dependencies(config)
         # GDB is not strictly a dependency, but having it in the disk image makes life a lot easier
         xtarget = cls.get_crosscompile_target()
-        gdb_xtarget = xtarget.get_cheri_hybrid_for_purecap_rootfs_target() if xtarget.is_cheri_purecap() else xtarget
+        gdb_xtarget = get_gdb_xtarget(xtarget, config)
         gdb_cls = get_build_gdb_class(gdb_xtarget, config)
         if gdb_xtarget in gdb_cls.supported_architectures():
             result += (gdb_cls.get_class_for_target(gdb_xtarget).target,)

tests/test_argument_parsing.py
@@ -126,9 +126,9 @@ def test_skip_update():
                 "llvm-native",
                 "bbl-baremetal-riscv64-purecap",
                 "cheribsd-riscv64-purecap",
-                "gmp-riscv64-hybrid-for-purecap-rootfs",
-                "mpfr-riscv64-hybrid-for-purecap-rootfs",
-                "gdb-riscv64-hybrid-for-purecap-rootfs",
+                "gmp-riscv64-purecap",
+                "mpfr-riscv64-purecap",
+                "gdb-riscv64-purecap",
                 "qemu",
                 "disk-image-riscv64-purecap",
                 "run-riscv64-purecap",

tests/test_target_order.py
@@ -237,9 +237,9 @@ def test_build_and_run(target_name: str, expected_list: "list[str]"):
             [
                 "morello-llvm-native",
                 "cheribsd-morello-purecap",
-                "gmp-morello-hybrid-for-purecap-rootfs",
-                "mpfr-morello-hybrid-for-purecap-rootfs",
-                "gdb-morello-hybrid-for-purecap-rootfs",
+                "gmp-morello-purecap",
+                "mpfr-morello-purecap",
+                "gdb-morello-purecap",
                 "qemu",
                 "disk-image-morello-purecap",
             ],
@@ -278,9 +278,9 @@ def test_build_and_run(target_name: str, expected_list: "list[str]"):
                 "llvm-native",
                 "bbl-baremetal-riscv64-purecap",
                 "cheribsd-riscv64-purecap",
-                "gmp-riscv64-hybrid-for-purecap-rootfs",
-                "mpfr-riscv64-hybrid-for-purecap-rootfs",
-                "gdb-riscv64-hybrid-for-purecap-rootfs",
+                "gmp-riscv64-purecap",
+                "mpfr-riscv64-purecap",
+                "gdb-riscv64-purecap",
                 "qemu",
                 "disk-image-riscv64-purecap",
             ],
@@ -334,9 +334,9 @@ def test_build_and_run(target_name: str, expected_list: "list[str]"):
                 "morello-firmware",
                 "morello-llvm-native",
                 "cheribsd-morello-purecap",
-                "gmp-morello-hybrid-for-purecap-rootfs",
-                "mpfr-morello-hybrid-for-purecap-rootfs",
-                "gdb-morello-hybrid-for-purecap-rootfs",
+                "gmp-morello-purecap",
+                "mpfr-morello-purecap",
+                "gdb-morello-purecap",
                 "disk-image-morello-purecap",
             ],
         ),
@@ -553,30 +553,28 @@ def test_webkit_cached_deps():
 
 
 def test_gdb_cached_deps():
-    target_names = _sort_targets(["gdb-morello-hybrid-for-purecap-rootfs"], add_dependencies=True, skip_sdk=True)
+    target_names = _sort_targets(["gdb-morello-purecap"], add_dependencies=True, skip_sdk=True)
     assert target_names == [
-        "gmp-morello-hybrid-for-purecap-rootfs",
-        "mpfr-morello-hybrid-for-purecap-rootfs",
-        "gdb-morello-hybrid-for-purecap-rootfs",
+        "gmp-morello-purecap",
+        "mpfr-morello-purecap",
+        "gdb-morello-purecap",
     ]
-    gdb_morello_hybrid = target_manager.get_target_raw("gdb-morello-hybrid-for-purecap-rootfs").project_class
+    gdb_morello = target_manager.get_target_raw("gdb-morello-purecap").project_class
     # LLVM and CheriBSD is not included in the list above since we did not request them, but full deps should include
-    all_names = list(sorted([x.name for x in gdb_morello_hybrid.cached_full_dependencies()]))
+    all_names = list(sorted([x.name for x in gdb_morello.cached_full_dependencies()]))
     assert all_names == [
         "cheribsd-morello-purecap",
-        "gmp-morello-hybrid-for-purecap-rootfs",
+        "gmp-morello-purecap",
         "morello-llvm-native",
-        "mpfr-morello-hybrid-for-purecap-rootfs",
+        "mpfr-morello-purecap",
     ]
-    sorted_by_order = _sort_targets(
-        [*all_names, "gdb-morello-hybrid-for-purecap-rootfs"], add_dependencies=False, skip_sdk=True
-    )
+    sorted_by_order = _sort_targets([*all_names, "gdb-morello-purecap"], add_dependencies=False, skip_sdk=True)
     assert sorted_by_order == [
         "morello-llvm-native",
         "cheribsd-morello-purecap",
-        "gmp-morello-hybrid-for-purecap-rootfs",
-        "mpfr-morello-hybrid-for-purecap-rootfs",
-        "gdb-morello-hybrid-for-purecap-rootfs",
+        "gmp-morello-purecap",
+        "mpfr-morello-purecap",
+        "gdb-morello-purecap",
     ]
 
 

commit 2dbca1e9deb9debd951adc1aedc2b44cf49b8089

gdb: Fix duplicate gdb-native when building on Morello
We currently rename all the (*-)gdb-native-hybrid to (*-)gdb-native, but
now that we can build gdb-*-purecap we don't want to be doing that on
Morello, as we also have a real gdb-native using that name. Push the
renaming down to the classes that override the list of supported
architectures to not include purecap ones.

Fixes: 076888d3a5c8 ("gdb: Support building CHERI GDB as purecap and switch default ABI to purecap")

pycheribuild/projects/cross/gdb.py
@@ -65,13 +65,6 @@ class BuildGDBBase(CrossCompileAutotoolsProject):
     _default_architecture = CompilationTargets.NATIVE
     prefer_full_lto_over_thin_lto = True
 
-    @staticmethod
-    def custom_target_name(base_target: str, xtarget: CrossCompileTarget) -> str:
-        if xtarget is CompilationTargets.NATIVE_NON_PURECAP and xtarget != CompilationTargets.NATIVE:
-            assert xtarget.generic_target_suffix == "native-hybrid", xtarget.generic_target_suffix
-            return base_target + "-native"
-        return base_target + "-" + xtarget.generic_target_suffix
-
     @classmethod
     def dependencies(cls, config: CheriConfig) -> "tuple[str, ...]":
         deps = super().dependencies(config)
@@ -283,6 +276,13 @@ class BuildUpstreamGDB(BuildGDBBase):
     )
     _default_architecture = CompilationTargets.NATIVE_NON_PURECAP
 
+    @staticmethod
+    def custom_target_name(base_target: str, xtarget: CrossCompileTarget) -> str:
+        if xtarget is CompilationTargets.NATIVE_NON_PURECAP and xtarget != CompilationTargets.NATIVE:
+            assert xtarget.generic_target_suffix == "native-hybrid", xtarget.generic_target_suffix
+            return base_target + "-native"
+        return base_target + "-" + xtarget.generic_target_suffix
+
 
 class BuildGDB(BuildGDBBase):
     path_in_rootfs = "/usr/local"  # Always install gdb as /usr/local/bin/gdb
@@ -320,6 +320,13 @@ class BuildCheriAllianceGDB(BuildGDBBase):
         CompilationTargets.NATIVE_NON_PURECAP,
     )
     _default_architecture = CompilationTargets.NATIVE_NON_PURECAP
+
+    @staticmethod
+    def custom_target_name(base_target: str, xtarget: CrossCompileTarget) -> str:
+        if xtarget is CompilationTargets.NATIVE_NON_PURECAP and xtarget != CompilationTargets.NATIVE:
+            assert xtarget.generic_target_suffix == "native-hybrid", xtarget.generic_target_suffix
+            return base_target + "-native"
+        return base_target + "-" + xtarget.generic_target_suffix
 
 
 class BuildKGDB(BuildGDB):