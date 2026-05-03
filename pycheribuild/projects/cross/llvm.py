6f5b2cea228ece9f322cda520fb39b8fda1a023a
Hesham Almatary <heshamelmatary@users.noreply.github.com>
2026-05-03 17:04:20 +0100

    alliance-llvm: switch to 19.1.7 branch (#488)
    https://github.com/CHERI-Alliance/llvm-project/pull/18 is now incorporating in
    the latest/default Alliance's 19.1.7 branch

--- pycheribuild/projects/cross/llvm.py
+++ pycheribuild/projects/cross/llvm.py
@@ -821,12 +821,8 @@ class BuildMorelloLLVM(BuildLLVMMonoRepoBase):
 class BuildCheriAllianceLLVM(BuildLLVMMonoRepoBase):
     repository = GitRepository(
         "https://github.com/CHERI-Alliance/llvm-project.git",
-        # TODO: Use the previous default once it can build CheriBSD:
-        # default_branch="codasip-cheri-riscv",
-        # https://github.com/CHERI-Alliance/llvm-project/pull/18
-        default_branch="cheribsd-rvy-intrinsics-19",
+        default_branch="codasip-cheri-riscv-19.1.7",
         force_branch=True,
-        temporary_url_override="https://github.com/qwattash/cheri-alliance-llvm-project.git",
     )
     default_directory_basename = "cheri-std093-llvm-project"
     target = "cheri-std093-llvm"
