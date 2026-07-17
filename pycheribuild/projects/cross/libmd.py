ab22f34c308effec1dcde6bf5615e6ad05d16400
Paul Metzger <paul.metzger@cl.cam.ac.uk>
2026-07-17 11:10:53 +0100

    Install shared library dependencies in the /lib
    These libraries most be copied from the cross-compile sysroot
    to rootfs/lib.

--- pycheribuild/projects/cross/libmd.py
+++ pycheribuild/projects/cross/libmd.py
@@ -63,3 +63,10 @@ class BuildLibmd(CrossCompileAutotoolsProject):
         self.COMMON_LDFLAGS.append("--unwindlib=none")
         # Remove dependcy on libgcc_s
         self.COMMON_LDFLAGS.append("-Wc,--unwindlib=none")
+
+    def install(self, **kwargs) -> None:
+        self.run_make_install()
+
+        # Copy the libraries from the cross-compile sysroot into rootfs/lib
+        for sofile in self.install_dir.glob("lib/libmd.so*"):
+            self.install_file(sofile, self.install_dir / f"rootfs/lib/" / sofile.name, create_dirs=True)
