6d94c8b834b834f6b94845367bac14d4009ea1b9
Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>
2026-07-24 15:20:39 +0100

    cheri-sel4-excercises: pass on board as an argument for scripts

--- pycheribuild/projects/cross/cheri_sel4_exercises.py
+++ pycheribuild/projects/cross/cheri_sel4_exercises.py
@@ -23,6 +23,7 @@ from .cheri_microkit import BuildCheriMicrokit
 from .crosscompileproject import CompilationTargets, CrossCompileProject, DefaultInstallDir, GitRepository
 from ..build_qemu import BuildCheriAllianceQEMU, BuildQEMU
 from ..run_qemu import LaunchQEMUBase
+from ..simple_project import OptionalStringConfigOption
 from ...config.chericonfig import RiscvCheriISA
 from ...qemu_utils import QemuOptions
 
@@ -74,13 +75,21 @@ class BuildCheriseL4Excercises(CrossCompileProject):
     # ------------------------------------------------------------------
     # Project config
     # ------------------------------------------------------------------
+    board = OptionalStringConfigOption(
+        "board",
+        metavar="BOARD",
+        show_help=True,
+        default=None,
+        help="CHERI-Microkit board to build against.",
+    )
+
     def configure(self, **kwargs) -> None:
         if self.crosscompile_target.is_riscv(include_purecap=True):
-            self.board = "qemu_virt_riscv64"
+            self.board = "qemu_virt_riscv64" if self.board is None else self.board
             # Build and test both baseline and purecap variants
             self.targets = ["riscv64", "riscv64-purecap"]
         elif self.crosscompile_target.is_aarch64(include_purecap=True):
-            self.board = "morello_qemu"
+            self.board = "morello_qemu" if self.board is None else self.board
             self.targets = ["morello-aarch64", "morello-purecap"]
 
     def needs_configure(self) -> bool:
@@ -133,7 +142,8 @@ class BuildCheriseL4Excercises(CrossCompileProject):
         if self.crosscompile_target.is_riscv(include_purecap=True):
             flags.append("-G0")
 
-        cmd: "list[str | Path]" = [ccc, target]
+        assert self.board is not None
+        cmd: "list[str | Path]" = [ccc, target, self.board]
         cmd.extend(flags)
         cmd.extend(sources)
         cmd.extend(["-o", output_elf])
@@ -203,12 +213,15 @@ class BuildCheriseL4Excercises(CrossCompileProject):
         for target in self.targets:
             # print-pointer
             elf = self.real_install_root_dir / "print-pointer.elf"
-            self.run_cmd([ccc, target, src_dir / "print-pointer.c", "-o", elf])
+
+            self.run_cmd([ccc, target, self.board, src_dir / "print-pointer.c", "-o", elf])
             self.run_cmd(
                 [
                     self._gen_image(),
                     "-a",
                     target,
+                    "-b",
+                    self.board,
                     "-o",
                     self.real_install_root_dir / f"print-pointer-cheri-sel4-microkit-{target}-{self.board}.img",
                     elf,
@@ -219,12 +232,14 @@ class BuildCheriseL4Excercises(CrossCompileProject):
             # print-capability (purecap only)
             if "purecap" in target:
                 elf = self.real_install_root_dir / "print-capability.elf"
-                self.run_cmd([ccc, target, src_dir / "print-capability.c", "-o", elf])
+                self.run_cmd([ccc, target, self.board, src_dir / "print-capability.c", "-o", elf])
                 self.run_cmd(
                     [
                         self._gen_image(),
                         "-a",
                         target,
+                        "-b",
+                        self.board,
                         "-o",
                         self.real_install_root_dir / f"print-capability-cheri-sel4-microkit-{target}-{self.board}.img",
                         elf,
