a943b7e352184768d9242f09b670684460467cad
Paul Metzger <paul.metzger@cl.cam.ac.uk>
2026-07-24 14:15:35 +0100

    Set OBJCOPY for cheriostest
    The installation of libcheribsdtest_dynamic failed when
    cross-compiling on Ubuntu because the local objcopy was used
    instead of CHERI LLVM's objcopy. This commit fixes this by setting
    the OBJCOPY environment variable. Add the property objcopy to
    _ClangBasedTargetInfo to support this.

--- pycheribuild/projects/cross/cheri_os_test.py
+++ pycheribuild/projects/cross/cheri_os_test.py
@@ -85,6 +85,8 @@ class BuildCheriOSTest(CrossCompileMakefileProject):
             # This is not supported by Morello LLVM,
             MAKESYSPATH=str(self.source_dir / "mk"),
             MAKEOBJDIRPREFIX=str(self.build_dir),
+            # This property was added to _ClangBasedTargetInfo to support this specific use case.
+            OBJCOPY=self.target_info.objcopy,
         )
 
         self.run_make(cwd=self.source_dir / "cheriostest")
