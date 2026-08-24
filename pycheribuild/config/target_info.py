23f915c7f543e74e6301e9b2e14217e1afa5a181
Jessica Clarke <jrtc27@jrtc27.com>
2026-08-24 13:23:14 +0100

    target_info.py: Use correct TLS flags for Morello on CheriBSD 20260206+
    Note that 20260206 never left the next branch, but is what the ports
    wrapper checks.

--- pycheribuild/config/target_info.py
+++ pycheribuild/config/target_info.py
@@ -1290,6 +1290,10 @@ def _cheribsd_morello_version_dependent_flags(cheribsd_version: int, is_purecap:
         result.extend(["--start-no-unused-arguments", "-Wl,--local-caprelocs=elf", "--end-no-unused-arguments"])
     if cheribsd_version >= 20250301:
         result.append("-cheri-codeptr-relocs")
+    if cheribsd_version >= 20260801:
+        result.append("-cheri-tgot-tls")
+    elif cheribsd_version >= 20260206:
+        result.append("-cheri-tgot-tls=compat")
     return result
 
 
