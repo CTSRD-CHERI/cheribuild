CTSRD-CHERI/cheribuild

commit 99a38fafb50f860249f470773aa6fc73736d42ed

Avoid install directory conflict for GDB
Hybrid-for-purecap-rootfs and purepca were both installing to /usr/local.
Add a symlink to the expected one in /usr/local/bin instead.

pycheribuild/projects/cross/gdb.py
@@ -50,7 +50,7 @@ from ...utils import OSInfo
 
 
 class BuildGDBBase(CrossCompileAutotoolsProject):
-    cross_install_dir = DefaultInstallDir.ROOTFS_OPTBASE
+    cross_install_dir = DefaultInstallDir.ROOTFS_LOCALBASE
     repository = GitRepository("git://sourceware.org/git/binutils-gdb.git")
     make_kind = MakeCommandKind.GnuMake
     do_not_add_to_targets = True
@@ -291,7 +291,7 @@ class BuildUpstreamGDB(BuildGDBBase):
 
 
 class BuildGDB(BuildGDBBase):
-    path_in_rootfs = "/usr/local"  # Always install gdb as /usr/local/bin/gdb
+    # path_in_rootfs = "/usr/local"
     native_install_dir = DefaultInstallDir.CHERI_SDK
     default_branch = "cheri-14"
     repository = GitRepository(
@@ -308,6 +308,14 @@ class BuildGDB(BuildGDBBase):
         },
         old_urls=["https://github.com/bsdjhb/gdb.git"],
     )
+
+    def install(self, **kwargs):
+        super().install(**kwargs)
+        # Always install gdb as /usr/local/bin/gdb, so it's in $PATH
+        if not self.compiling_for_host() and self.crosscompile_target == get_gdb_xtarget(
+            self.crosscompile_target, self.config
+        ):
+            self.create_symlink(self.install_prefix / "bin/gdb", self.rootfs_dir / "usr/local/bin/gdb", relative=False)
 
 
 class BuildCheriAllianceGDB(BuildGDBBase):

commit b64c550531bb687b1df99f55242ef7a99b95b373

gdb: Fix installing for FreeBSD
For CheriBSD, ROOTFS_LOCALBASE installs to /usr/local/<arch>, and we
create a symlink in /usr/local/bin if building the default architecture,
but for FreeBSD, ROOTFS_LOCALBASE installs directly to /usr/local, so
creating the symlink overwrites the actual gdb binary with a symlink to
itself. Detect this case and skip the unwanted symlink.

Fixes: 99a38fafb50f ("Avoid install directory conflict for GDB")

pycheribuild/projects/cross/gdb.py
@@ -317,8 +317,12 @@ class BuildGDB(BuildGDBBase):
     def install(self, **kwargs):
         super().install(**kwargs)
         # Always install gdb as /usr/local/bin/gdb, so it's in $PATH
-        if not self.compiling_for_host() and self.crosscompile_target == get_gdb_xtarget(
-            self.crosscompile_target, self.config
+        # NB: Only CheriBSD uses /usr/local/<arch>, FreeBSD uses /usr/local,
+        # so don't want to replace /usr/local/bin/gdb with a symlink to itself.
+        if (
+            not self.compiling_for_host()
+            and self.crosscompile_target == get_gdb_xtarget(self.crosscompile_target, self.config)
+            and self.real_install_root_dir != self.rootfs_dir / "usr/local"
         ):
             self.makedirs(self.rootfs_dir / "usr/local/bin")
             self.create_symlink(self.install_prefix / "bin/gdb", self.rootfs_dir / "usr/local/bin/gdb", relative=False)